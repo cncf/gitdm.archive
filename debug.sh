7fb1e51cbab7ea868e5c4dae7d204a8dbb5a51a8
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-04-19 08:26:32 +0200

    lukasz-added-example-result-files

--- debug.sh
+++ debug.sh
@@ -2,10 +2,12 @@
 PWD=`pwd`
 cd ~/dev/kubernetes/kubernetes/
 git config merge.renameLimit 10000
+git config diff.renameLimit 10000
 #git log -p -M | cncfdm.py -b /Users/mac/dev/gitdm/ -X -t -z -d -D -U -u -h first_run_patch.html -o first_run_patch.txt -x first_run_patch.csv
 #git log -p -M > git-full.log
-cncfdm.py -i git.log -b /Users/mac/dev/gitdm/ -X -t -z -d -D -U -m -h first_run_patch.html -o first_run_patch.txt -x first_run_patch.csv
+cncfdm.py -i git-full.log -b /Users/mac/dev/gitdm/ -X -t -z -d -D -U -m -h first_run_patch.html -o first_run_patch.txt -x first_run_patch.csv
 #cncfdm.py -i git-numstat.log -n -b /Users/mac/dev/gitdm/ -X -t -z -d -D -U -u -h first_run_patch.html -o first_run_patch.txt -x first_run_patch.csv
+git config --unset diff.renameLimit
 git config --unset merge.renameLimit
 cp first_run.txt ~/dev/gitdm/
 cd $PWD
--- run_for_rels.sh
+++ run_for_rels.sh
@@ -5,10 +5,12 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 PWD=`pwd`
-cd ~/dev/kubernetes/
+cd ~/dev/kubernetes/kubernetes/
 git config merge.renameLimit 10000
+git config diff.renameLimit 10000
 git log -p -M $1..$2 | cncfdm.py -b /Users/mac/dev/gitdm/ -t -z -d -D -U -m -h output_patch.html -o output_patch.txt -x output_patch.csv
 git log --numstat -M $1..$2 | cncfdm.py -n -b /Users/mac/dev/gitdm/ -t -z -d -D -U -m -h output_numstat.html -o output_numstat.txt -x output_numstat.csv > output_numstat.out
+git config --unset diff.renameLimit
 git config --unset merge.renameLimit
 ls -l output_*
 cd $PWD
