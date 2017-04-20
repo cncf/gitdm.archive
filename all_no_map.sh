ffdce62c979d51f8ca0d035c626efd27779ef199
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-04-20 13:18:11 +0200

    lukaszgryglicki-with-individual-developers

---
+++ all_no_map.sh
@@ -0,0 +1,2 @@
+./run_no_map.sh
+cp run_no_map_* ~/dev/cncf/gitdm/kubernetes/all_time/
---
+++ run_no_map.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+PWD=`pwd`
+cd ~/dev/kubernetes/kubernetes/
+git config merge.renameLimit 10000
+git config diff.renameLimit 10000
+# -m --> map unknowns to 'DomainName *' , -u map unknowns to '(Unknown)'
+git log -p -M | /Users/mac/dev/cncf/gitdm/cncfdm.py -b /Users/mac/dev/cncf/gitdm/ -t -z -d -D -U -h run_no_map_patch.html -o run_no_map_patch.txt -x run_no_map_patch.csv
+git log --numstat -M | /Users/mac/dev/cncf/gitdm/cncfdm.py -n -b /Users/mac/dev/cncf/gitdm/ -t -z -d -D -U -h run_no_map_numstat.html -o run_no_map_numstat.txt -x run_no_map_numstat.csv > run_no_map_numstat.out
+git config --unset diff.renameLimit
+git config --unset merge.renameLimit
+cd $PWD
+
