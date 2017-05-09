3dce5c061d84b4b3531b9ede86d29cc66700ae61
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-09 11:29:46 +0200

    lukaszgryglicki-new-data-run6

--- analysis.rb
+++ analysis.rb
@@ -94,7 +94,7 @@ def analysis(args)
     end
 
     fn = "report/#{prefix}_#{key}_top.csv"
-    required = ['(Unknown)', 'Gmail *', 'Qq *', 'Outlook *', 'Yahoo *', 'Hotmail *', 'Self', 'NotFound']
+    required = ['(Unknown)', 'Gmail *', 'Qq *', 'Outlook *', 'Yahoo *', 'Hotmail *', '(Self)', '(Not Found)']
     CSV.open(fn, "w", headers: hdr) do |csv|
       csv << hdr
       arr.each_with_index do |row, index|
