af12ea8c39de53a3b9b6ddb5ea9c0c8e4bb7e3b7
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-09 14:42:22 +0200

    lukaszgryglicki-updated-ruby-tool-to-generate-percents

--- topdevs.rb
+++ topdevs.rb
@@ -6,23 +6,34 @@ def analysis(fn)
   added = []
   removed = []
   changesets = []
+  sa = sr = sc = 0
   CSV.foreach(fn, headers: true) do |row|
     h = row.to_h
-    added << [h['Added'].to_i, h]
-    removed << [h['Removed'].to_i, h]
-    changesets << [h['Changesets'].to_i, h]
+    a = h['Added'].to_i
+    r = h['Removed'].to_i
+    c = h['Changesets'].to_i
+    sa += a
+    sr += r
+    sc += c
+    added << [a, h]
+    removed << [r, h]
+    changesets << [c, h]
   end
 
   added = added.sort_by { |item| -item[0] }
   removed = removed.sort_by { |item| -item[0] }
   changesets = changesets.sort_by { |item| -item[0] }
+  ks = added[0][1].keys + ['% Added', '% Removed', '% Changesets']
   %w(added removed changesets).each do |obj|
     fn = "#{obj}.csv"
     obj = binding.local_variable_get(obj)
-    CSV.open(fn, "w", headers: obj[0][1].keys) do |csv|
-      csv << obj[0][1].keys
+    CSV.open(fn, "w", headers: ks) do |csv|
+      csv << ks
       obj.each do |row|
-        csv << row[1].values
+        pa = (row[1]['Added'].to_f * 100.0 / sa).round(3)
+        pr = (row[1]['Removed'].to_f * 100.0 / sr).round(3)
+        pc = (row[1]['Changesets'].to_f * 100.0 / sc).round(3)
+        csv << row[1].values + [pa, pr, pc]
       end
     end
   end
