10a5a9f0bee2358a03fe774a4653c36e3ae0fae4
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-11 09:15:21 +0200

    lukaszgryglicki-updated-ruby-tool-to-count-per-company-new-developers

--- new_devs.rb
+++ new_devs.rb
@@ -3,20 +3,36 @@ require 'pry'
 
 def new_devs(files)
   known = {}
+  companies = {}
   files.each_with_index do |file, index|
     n = 0
     CSV.foreach(file, headers: true) do |row|
       h = row.to_h
       e = h['Email'].strip
+      c = h['Affliation'].strip
       known[e] = 1 if index == 0
-      if known.key?(e)
-        known[e] += 1
-      else
+      unless known.key?(e)
         known[e] = 1
         n += 1
+        companies[c] = 0 unless companies.key?(c)
+        companies[c] += 1
       end
     end
-    p n if index > 0
+    puts "#{index + 1}) #{file}: N=#{n}" if index > 0
+  end
+
+  arr = []
+  companies.each do |name, n|
+    arr << [name, n]
+  end
+  arr = arr.sort_by { |item| -item[1] }
+
+  ks = %w(company new_devs)
+  CSV.open("new_devs.csv", "w", headers: ks) do |csv|
+    csv << ks
+    arr.each do |row|
+      csv << row
+    end
   end
 end
 
