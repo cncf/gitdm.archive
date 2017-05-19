b63fb171ba43d4a2d7fd1d224825fe11a3ebc6cb
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-19 12:14:59 +0200

    lukaszgryglicki-v1.6-stats-added

--- topdevs.sh
+++ topdevs.sh
@@ -4,8 +4,13 @@ ruby topdevs.rb repos/combined.csv
 mv added.csv stats/all_added.csv
 mv removed.csv stats/all_removed.csv
 mv changesets.csv stats/all_changesets.csv
-echo "Stats kubernetes/kubernetes"
+echo "Stats kubernetes/kubernetes all time"
 ruby topdevs.rb kubernetes/all_time/first_run_patch.csv
 mv added.csv stats/kubernetes_added.csv
 mv removed.csv stats/kubernetes_removed.csv
 mv changesets.csv stats/kubernetes_changesets.csv
+echo "Stats kubernetes/kubernetes v1.6.0"
+ruby topdevs.rb kubernetes/v1.5.0-v1.6.0/output_strict_patch.csv
+mv added.csv stats/v1.6_added.csv
+mv removed.csv stats/v1.6_removed.csv
+mv changesets.csv stats/v1.6_changesets.csv
