01ddb69b352b36d959a85d0575442c73c90b5a1b
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-23 14:16:45 +0200

    lukaszgryglicki-added-script-to-return-number-of-authors-and-commits-for-external-repos

---
+++ repo_in_range.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PWD=`pwd`
+FN=$PWD/repos/$2_$3_$4
+cd "$1"
+echo "Processing repo $1 $2 $3 $4"
+git config merge.renameLimit 100000
+git config diff.renameLimit 100000
+git log --numstat -M --since "$3" --until "$4" | ~/dev/cncf/gitdm/cncfdm.py -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -u -f "$3" -e "$4" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
+git config --unset diff.renameLimit
+git config --unset merge.renameLimit
