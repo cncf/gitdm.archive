088fed840ceb59eec30903dda83b642a672f9fa6
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-02 09:32:17 +0200

    lukaszgryglicki-removed-3-orgs-from-k8s-added-script-to-git-pull-all-k8s-repos-rerun-data

--- kubernetes_repos.sh
+++ kubernetes_repos.sh
@@ -74,28 +74,29 @@
 ./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-client-community/ kubernetes-client-community
 ./anyrepo.sh ~/dev/go/src/k8s.io/go-base/ go-base
 ./anyrepo.sh ~/dev/go/src/k8s.io/go/ go
+# We no more need 3 kubernetes orgs: kubernetes-contrib, kubernetes-ui, kubernetes-cluster-automation
 # kubernetes-contrib org
 # application-images --> kubernetes-contrib-application-images (renamed due to name conflict)
-./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-contrib-application-images/ kubernetes-contrib-application-images
-./anyrepo.sh ~/dev/go/src/k8s.io/jumpserver/ jumpserver
-./anyrepo.sh ~/dev/go/src/k8s.io/graylog2/ graylog2
-./anyrepo.sh ~/dev/go/src/k8s.io/consul/ consul
+# ./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-contrib-application-images/ kubernetes-contrib-application-images
+# ./anyrepo.sh ~/dev/go/src/k8s.io/jumpserver/ jumpserver
+# ./anyrepo.sh ~/dev/go/src/k8s.io/graylog2/ graylog2
+# ./anyrepo.sh ~/dev/go/src/k8s.io/consul/ consul
 # kubernetes-cluster-automation org
-./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry/ docker-registry 
-./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-minion/ kubernetes-minion 
-./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-master/ kubernetes-master
-./anyrepo.sh ~/dev/go/src/k8s.io/flanneld/ flanneld
-./anyrepo.sh ~/dev/go/src/k8s.io/etcd/ etcd
-./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry-images/ docker-registry-images
-./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry-proxy/ docker-registry-proxy
-./anyrepo.sh ~/dev/go/src/k8s.io/docker-install/ docker-install
+# ./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry/ docker-registry 
+# ./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-minion/ kubernetes-minion 
+# ./anyrepo.sh ~/dev/go/src/k8s.io/kubernetes-master/ kubernetes-master
+# ./anyrepo.sh ~/dev/go/src/k8s.io/flanneld/ flanneld
+# ./anyrepo.sh ~/dev/go/src/k8s.io/etcd/ etcd
+# ./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry-images/ docker-registry-images
+# ./anyrepo.sh ~/dev/go/src/k8s.io/docker-registry-proxy/ docker-registry-proxy
+# ./anyrepo.sh ~/dev/go/src/k8s.io/docker-install/ docker-install
 # kubernetes-ui org
-./anyrepo.sh ~/dev/go/src/k8s.io/label-selector/ label-selector
-./anyrepo.sh ~/dev/go/src/k8s.io/object-describer/ object-describer
-./anyrepo.sh ~/dev/go/src/k8s.io/container-terminal/ container-terminal
-./anyrepo.sh ~/dev/go/src/k8s.io/topology-graph/ topology-graph
-./anyrepo.sh ~/dev/go/src/k8s.io/graph/ graph
-./anyrepo.sh ~/dev/go/src/k8s.io/kube-ui-docker/ kube-ui-docker
+# ./anyrepo.sh ~/dev/go/src/k8s.io/label-selector/ label-selector
+# ./anyrepo.sh ~/dev/go/src/k8s.io/object-describer/ object-describer
+# ./anyrepo.sh ~/dev/go/src/k8s.io/container-terminal/ container-terminal
+# ./anyrepo.sh ~/dev/go/src/k8s.io/topology-graph/ topology-graph
+# ./anyrepo.sh ~/dev/go/src/k8s.io/graph/ graph
+# ./anyrepo.sh ~/dev/go/src/k8s.io/kube-ui-docker/ kube-ui-docker
 
 echo "All repos combined"
 ./multirepo.sh ~/dev/go/src/k8s.io/test-infra/ ~/dev/go/src/k8s.io/kubernetes/ ~/dev/go/src/k8s.io/kubernetes.github.io/ ~/dev/go/src/k8s.io/contrib/ ~/dev/go/src/k8s.io/helm/ ~/dev/go/src/k8s.io/kops/ ~/dev/go/src/k8s.io/community/ ~/dev/go/src/k8s.io/heapster/ ~/dev/go/src/k8s.io/dashboard/ ~/dev/go/src/k8s.io/minikube/ ~/dev/go/src/k8s.io/charts/ ~/dev/go/src/k8s.io/kube-state-metrics/ ~/dev/go/src/k8s.io/kubernetes-anywhere/ ~/dev/go/src/k8s.io/ingress/ ~/dev/go/src/k8s.io/release/ ~/dev/go/src/k8s.io/gengo/ ~/dev/go/src/k8s.io/autoscaler/ ~/dev/go/src/k8s.io/dns/ ~/dev/go/src/k8s.io/sample-apiserver/ ~/dev/go/src/k8s.io/apiserver/ ~/dev/go/src/k8s.io/kube-aggregator/ ~/dev/go/src/k8s.io/client-go/ ~/dev/go/src/k8s.io/node-problem-detector/ ~/dev/go/src/k8s.io/perf-tests/ ~/dev/go/src/k8s.io/apimachinery/ ~/dev/go/src/k8s.io/frakti/ ~/dev/go/src/k8s.io/features/ ~/dev/go/src/k8s.io/repo-infra/ ~/dev/go/src/k8s.io/kube-deploy/ ~/dev/go/src/k8s.io/examples/ ~/dev/go/src/k8s.io/git-sync/ ~/dev/go/src/k8s.io/k8s.io/ ~/dev/go/src/k8s.io/kubernetes-bootcamp/ ~/dev/go/src/k8s.io/kubectl/ ~/dev/go/src/k8s.io/metrics/ ~/dev/go/src/k8s.io/md-check/ ~/dev/go/src/k8s.io/kubernetes-template-project/ ~/dev/go/src/k8s.io/kubernetes-docs-cn/ ~/dev/go/src/k8s.io/kube-ui/ ~/dev/go/src/k8s.io/kubeadm/ ~/dev/go/src/k8s.io/kompose/ ~/dev/go/src/k8s.io/external-storage/ ~/dev/go/src/k8s.io/cri-tools/ ~/dev/go/src/k8s.io/kube-aws/ ~/dev/go/src/k8s.io/external-dns/ ~/dev/go/src/k8s.io/bootkube/ ~/dev/go/src/k8s.io/service-catalog/ ~/dev/go/src/k8s.io/kargo/ ~/dev/go/src/k8s.io/cri-o/ ~/dev/go/src/k8s.io/cri-containerd/ ~/dev/go/src/k8s.io/apiserver-builder/ ~/dev/go/src/k8s.io/ip-masq-agent/ ~/dev/go/src/k8s.io/client-python/ ~/dev/go/src/k8s.io/cluster-capacity/ ~/dev/go/src/k8s.io/reference-docs/ ~/dev/go/src/k8s.io/kube-mesos-framework/ ~/dev/go/src/k8s.io/rktlet/ ~/dev/go/src/k8s.io/spartakus/ ~/dev/go/src/k8s.io/cluster-proportional-autoscaler/ ~/dev/go/src/k8s.io/nfs-provisioner/ ~/dev/go/src/k8s.io/node-feature-discovery/ ~/dev/go/src/k8s.io/application-images/ ~/dev/go/src/k8s.io/java/ ~/dev/go/src/k8s.io/gen/ ~/dev/go/src/k8s.io/python-base/ ~/dev/go/src/k8s.io/csharp/ ~/dev/go/src/k8s.io/ruby/ ~/dev/go/src/k8s.io/javascript/ ~/dev/go/src/k8s.io/kubernetes-client-community/ ~/dev/go/src/k8s.io/go-base/ ~/dev/go/src/k8s.io/go/ ~/dev/go/src/k8s.io/kubernetes-contrib-application-images/ ~/dev/go/src/k8s.io/jumpserver/ ~/dev/go/src/k8s.io/graylog2/ ~/dev/go/src/k8s.io/consul/ ~/dev/go/src/k8s.io/docker-registry/ ~/dev/go/src/k8s.io/kubernetes-minion/ ~/dev/go/src/k8s.io/kubernetes-master/ ~/dev/go/src/k8s.io/flanneld/ ~/dev/go/src/k8s.io/etcd/ ~/dev/go/src/k8s.io/docker-registry-images/ ~/dev/go/src/k8s.io/docker-registry-proxy/ ~/dev/go/src/k8s.io/docker-install/ ~/dev/go/src/k8s.io/label-selector/ ~/dev/go/src/k8s.io/object-describer/ ~/dev/go/src/k8s.io/container-terminal/ ~/dev/go/src/k8s.io/topology-graph/ ~/dev/go/src/k8s.io/graph/ ~/dev/go/src/k8s.io/kube-ui-docker/
