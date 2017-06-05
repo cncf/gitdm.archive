9def953d84905977833530d57a0f0dd3472dfc1c
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-05 08:10:10 +0200

    lukaszgryglicki-linkerd-fluentd

--- join_stats.sh
+++ join_stats.sh
@@ -3,3 +3,5 @@ echo "All CNCF Projects Join statistics (authors, commits, additions, removals) 
 ./cncf_join_analysis.sh prometheus 2016-05-09 90 ~/dev/prometheus/
 ./cncf_join_analysis.sh kubernetes 2016-03-10 90 ~/dev/kubernetes_repos_links
 ./cncf_join_analysis.sh opentracing 2016-08-17 90 ~/dev/opentracing/
+./cncf_join_analysis.sh fluentd 2016-08-03 90 ~/dev/fluentd/
+./cncf_join_analysis.sh linkerd 2016-10-05 90 ~/dev/linkerd/
