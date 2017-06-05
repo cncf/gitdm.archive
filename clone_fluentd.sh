f2a4e4285b95f5feae96eb780d807aa3b3a80524
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-05 08:10:24 +0200

    lukaszgryglicki-linkerd-fluentd-new-files

---
+++ clone_fluentd.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+mkdir ~/dev/fluentd/ 2>/dev/null
+# This list is from `cncf/velocity`:`BigQuery/query_prometheus_repos.sql`
+cd ~/dev/fluentd || exit 1
+git clone https://github.com/fluent/NLog.Targets.Fluentd.git || exit 1
+git clone https://github.com/fluent/fluent-bit.git || exit 1
+git clone https://github.com/fluent/fluent-bit-docker-image.git || exit 1
+git clone https://github.com/fluent/fluent-bit-docs.git || exit 1
+git clone https://github.com/fluent/fluent-bit-go.git || exit 1
+git clone https://github.com/fluent/fluent-bit-kubernetes-daemonset.git || exit 1
+git clone https://github.com/fluent/fluent-logger-erlang.git || exit 1
+git clone https://github.com/fluent/fluent-logger-golang.git || exit 1
+git clone https://github.com/fluent/fluent-logger-java.git || exit 1
+git clone https://github.com/fluent/fluent-logger-node.git || exit 1
+git clone https://github.com/fluent/fluent-logger-perl.git || exit 1
+git clone https://github.com/fluent/fluent-logger-php.git || exit 1
+git clone https://github.com/fluent/fluent-logger-python.git || exit 1
+git clone https://github.com/fluent/fluent-logger-ruby.git || exit 1
+git clone https://github.com/fluent/fluent-logger-scala.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-flume.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-grok-parser.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-kafka.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-mongo.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-multiprocess.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-rewrite-tag-filter.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-s3.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-sql.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-webhdfs.git || exit 1
+git clone https://github.com/fluent/fluent-plugin-windows-eventlog.git || exit 1
+git clone https://github.com/fluent/fluentd.git || exit 1
+git clone https://github.com/fluent/fluentd-docker-image.git || exit 1
+git clone https://github.com/fluent/fluentd-docs.git || exit 1
+git clone https://github.com/fluent/fluentd-forwarder.git || exit 1
+git clone https://github.com/fluent/fluentd-kubernetes-daemonset.git || exit 1
+git clone https://github.com/fluent/fluentd-ui.git || exit 1
+git clone https://github.com/fluent/fluentd-website.git || exit 1
+git clone https://github.com/fluent/serverengine.git || exit 1
+echo "All Fluentd repos cloned"
---
+++ clone_linkerd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir ~/dev/linkerd/ 2>/dev/null
+# This list is from `cncf/velocity`:`BigQuery/query_prometheus_repos.sql`
+cd ~/dev/linkerd || exit 1
+git clone https://github.com/linkerd/linkerd.git || exit 1
+git clone https://github.com/linkerd/linkerd-examples.git || exit 1
+git clone https://github.com/linkerd/linkerd-tcp.git || exit 1
+git clone https://github.com/linkerd/linkerd-viz.git || exit 1
+git clone https://github.com/linkerd/linkerd-zipkin.git || exit 1
+git clone https://github.com/linkerd/namerctl.git || exit 1
+git clone https://github.com/linkerd/rustup-nightly-docker.git || exit 1
+git clone https://github.com/linkerd/tacho.git || exit 1
+echo "All Linkerd repos cloned"
