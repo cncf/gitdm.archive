e4ea1b0dac86c8e971b99c1c83b83cfb53ff130c
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-05 12:48:13 +0200

    lukaszgryglicki-prometheus-longer-stats

--- join_stats.sh
+++ join_stats.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "All CNCF Projects Join statistics (authors, commits, additions, removals) 90 days before and after joining CNCF"
 ./cncf_join_analysis.sh prometheus 2016-05-09 90 ~/dev/prometheus/
+./cncf_join_analysis.sh prometheus_6m 2016-05-09 183 ~/dev/prometheus/
+./cncf_join_analysis.sh prometheus_1y 2016-05-09 365 ~/dev/prometheus/
 ./cncf_join_analysis.sh kubernetes 2016-03-10 90 ~/dev/kubernetes_repos_links
 ./cncf_join_analysis.sh opentracing 2016-08-17 90 ~/dev/opentracing/
 ./cncf_join_analysis.sh fluentd 2016-08-03 90 ~/dev/fluentd/
