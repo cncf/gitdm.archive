aa710a1f6da4be1e8399685277374468d3ee8b83
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-13 12:56:31 +0200

    lukaszgryglicki-truncate-topdevs-output

--- topdevs.sh
+++ topdevs.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 echo "Stats all repos"
 ruby topdevs.rb repos/combined.csv
-mv added.csv stats/all_added.csv
-mv removed.csv stats/all_removed.csv
-mv changesets.csv stats/all_changesets.csv
+head -n 101 added.csv > stats/all_added.csv
+head -n 101 removed.csv > stats/all_removed.csv
+head -n 101 changesets.csv > stats/all_changesets.csv
 echo "Stats kubernetes/kubernetes all time"
 ruby topdevs.rb kubernetes/all_time/first_run_patch.csv
-mv added.csv stats/kubernetes_added.csv
-mv removed.csv stats/kubernetes_removed.csv
-mv changesets.csv stats/kubernetes_changesets.csv
+head -n 101 added.csv > stats/kubernetes_added.csv
+head -n 101 removed.csv > stats/kubernetes_removed.csv
+head -n 101 changesets.csv > stats/kubernetes_changesets.csv
 echo "Stats kubernetes/kubernetes v1.6.0"
 ruby topdevs.rb kubernetes/v1.5.0-v1.6.0/output_strict_patch.csv
-mv added.csv stats/v1.6_added.csv
-mv removed.csv stats/v1.6_removed.csv
-mv changesets.csv stats/v1.6_changesets.csv
+head -n 101 added.csv > stats/v1.6_added.csv
+head -n 101 removed.csv > stats/v1.6_removed.csv
+head -n 101 changesets.csv > stats/v1.6_changesets.csv
 echo "Stats kubernetes/kubernetes v1.7.0"
 ruby topdevs.rb kubernetes/v1.6.0-v1.7.0/output_strict_patch.csv
-mv added.csv stats/v1.7_added.csv
-mv removed.csv stats/v1.7_removed.csv
-mv changesets.csv stats/v1.7_changesets.csv
+head -n 101 added.csv > stats/v1.7_added.csv
+head -n 101 removed.csv > stats/v1.7_removed.csv
+head -n 101 changesets.csv > stats/v1.7_changesets.csv
+rm -f added.csv removed.csv changesets.csv
