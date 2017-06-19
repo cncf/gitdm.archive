243366a0c8e3c87b5f7b8a7d42dea42fc700ef3f
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-19 14:09:54 +0200

    lukaszgryglicki-github-commits-analysis-using-api-2-of-3-stages-completed

--- ghusers.rb
+++ ghusers.rb
@@ -2,6 +2,10 @@ require 'pry'
 require 'octokit'
 require 'json'
 
+# Ask each repo for commits newer than...
+start_date = '2014-01-01'
+
+# List of repositories to retrieve commits from (and get their basic data)
 repos = [
   'kubernetes/test-infra',
   'kubernetes/kubernetes',
@@ -75,46 +79,104 @@ repos = [
   'kubernetes-client/go'
 ]
 
-Octokit.auto_paginate = true
-client = Octokit::Client.new access_token: File.read('/etc/github/oauth').strip
-user = client.user
-user.login
+# It tries to call GitHub only twice per repo (general repo data & commits), then saves data JSON to files
+# If file is saved (2 per repo) - it is read isnstead of quering GitHub
+def rate_limit()
+  rl = Octokit.rate_limit
+  puts "Your rate limit is: limit=#{rl.limit}, remaining=#{rl.remaining}, resets_at=#{rl.resets_at}, resets_in=#{rl.resets_in}"
+  (rl.resets_at - Time.now).to_i + 1
+end
 
-Octokit.client_id = File.read('/etc/github/client_id').strip
-Octokit.client_secret = File.read('/etc/github/client_secret').strip
+def ghusers(repos, start_date)
+  # Auto paginate results, this uses maximum page size 100 internally and calls API # of results / 100 times.
+  Octokit.auto_paginate = true
 
-rl = Octokit.rate_limit
-puts "Your rate limit is: limit=#{rl.limit}, remaining=#{rl.remaining}, resets_at=#{rl.resets_at}, resets_in=#{rl.resets_in}"
-puts "Type exit-program if You want to exit"
-binding.pry
+  # Login with standard OAuth token
+  # https://github.com/settings/tokens --> Persona access tokens
+  client = Octokit::Client.new access_token: File.read('/etc/github/oauth').strip
+  user = client.user
+  user.login
 
-repos.each do |repo_name|
-  begin
-    puts "Processing #{repo_name}"
-    fn = 'ghusers/' + repo_name.gsub('/', '__')
-    f = File.read(fn)
-    puts "Got JSON from saved file"
-  rescue Errno::ENOENT => err1
+  # Increase rate limit from 60 to 5000
+  # You will need Your own client_id & client_secret
+  # See: https://github.com/settings/ --> OAuth application
+  Octokit.client_id = File.read('/etc/github/client_id').strip
+  Octokit.client_secret = File.read('/etc/github/client_secret').strip
+  # user = Octokit.user 'some_github_username'
+
+  rate_limit()
+  puts "Type exit-program if You want to exit"
+  # This is to ensure You want to continue, it displays Your limit, should be close to 5000
+  # If not type 'exit-program' if Yes type 'quit' (to quit debugger & continue)
+  binding.pry
+
+  # Process repositories general info
+  hs = []
+  repos.each do |repo_name|
     begin
-      puts "No previously saved #{fn}, getting repo from GitHub"
-      repo = Octokit.repo repo_name
-      json = JSON.pretty_generate repo.to_h
-      File.write fn, json
-    rescue Octokit::TooManyRequests => err2
-      rl = Octokit.rate_limit
-      td = (rl.resets_at - Time.now).to_i + 1
-      puts "Too many GitHub requests, sleeping for #{td} seconds"
-      sleep td
-      retry
-    rescue => err2
-      puts "Uups, somethis bad happened, check `err2` variable!"
-      binding.pry
+      puts "Processing #{repo_name}"
+      fn = 'ghusers/' + repo_name.gsub('/', '__')
+      f = File.read(fn)
+      puts "Got repository JSON from saved file"
+      h = JSON.parse f
+      hs << h
+    rescue Errno::ENOENT => err1
+      begin
+        puts "No previously saved #{fn}, getting repo from GitHub"
+        rate_limit()
+        repo = Octokit.repo repo_name
+        h = repo.to_h
+        json = JSON.pretty_generate h
+        File.write fn, json
+        hs << h
+      rescue Octokit::TooManyRequests => err2
+        td = rate_limit()
+        puts "Too many GitHub requests, sleeping for #{td} seconds"
+        sleep td
+        retry
+      rescue => err2
+        puts "Uups, somethis bad happened, check `err2` variable!"
+        binding.pry
+      end
     end
   end
-end
-binding.pry
 
-# user = Octokit.user 'jbarnette'
-# repo = Octokit.repo 'pengwynn/pingwynn'
-# Octokit.commits_since('kubernetes/test-infra', '2017-06-19')
-# repo.to_h.to_json
+  # Process each repository's commits
+  # 56k commits took 162/5000 points
+  # After processed all 70 repos I had xxxx/5000 points remaining
+  comms = []
+  hs.each do |repo|
+    begin
+      repo_name = repo['full_name'] || repo[:full_name]
+      puts "Getting commits from #{repo_name}"
+      fn = 'ghusers/' + repo_name.gsub('/', '__') + '__commits'
+      f = File.read(fn)
+      puts "Got commits JSON from saved file"
+      comm = JSON.parse f
+      comms << comm
+    rescue Errno::ENOENT => err1
+      begin
+        puts "No previously saved #{fn}, getting commits from GitHub"
+        rate_limit()
+        comm = Octokit.commits_since(repo_name, start_date)
+        h = comm.map(&:to_h)
+        puts "Got #{h.count} commits"
+        json = JSON.pretty_generate h
+        File.write fn, json
+        comms << comm
+      rescue Octokit::TooManyRequests => err2
+        td = rate_limit()
+        puts "Too many GitHub requests, sleeping for #{td} seconds"
+        binding.pry
+        sleep td
+        retry
+      rescue => err2
+        puts "Uups, somethis bad happened, check `err2` variable!"
+        binding.pry
+      end
+    end
+  end
+  binding.pry
+end
+
+ghusers(repos, start_date)
