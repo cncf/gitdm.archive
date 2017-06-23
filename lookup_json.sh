07c599ad340f67cfe9529f3fd4222722e3a1d12f
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-23 10:57:59 +0200

    lukaszgryglicki-regenerated-data

--- lookup_json.sh
+++ lookup_json.sh
@@ -4,4 +4,4 @@ ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.
 # Devs with Unknown affiliation having case insensitive "linkedin" in their blog or bio or location:
 ruby lookup_json.rb github_users.json ':any?, blog, location, bio' '/linkedin/i' affiliation '/^\(Unknown\)$/' unknown_with_linkedin.json
 # Devs with Unknown affiliation having blog property non-empty
-ruby lookup_json.rb github_users.json blog '/[^\\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
+ruby lookup_json.rb github_users.json blog '/[^\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
