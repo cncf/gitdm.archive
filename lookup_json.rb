c1f4553f44f24201b5aa30990f4ee3289205aecd
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-23 10:12:47 +0200

    lukaszgryglicki-added-comments

--- lookup_json.rb
+++ lookup_json.rb
@@ -2,20 +2,25 @@ require 'pry'
 require 'to_regexp'
 require 'json'
 
-def lookup_json(json_file, args)
+def lookup_json(json_file, args, output_json)
   # Parse RegExp filter(s)
   filters = {}
   key = nil
   args.each_with_index do |arg, index|
     if index % 2 == 0
+      # Column name(s), 1st arg can be ruby method name if starts with ':'
+      # So key can be: 
+      # column name 'blog'
+      # multiple column names 'login,bio, blog' - in this case default ruby operator is :all? which means all columns must match regexp
+      # multiple column names with operator: ':any?,blog,bio' - means that blog OR bio must match regexp
       key = arg.split(',').map(&:strip)
     else
+      # And now regexp to check on defined columns
       filters[key] = arg.to_regexp
     end
   end
-  output_json = args[-1]
 
-  # Parse JSON
+  # Parse input JSON
   data = JSON.parse File.read json_file
 
   # Filter json
@@ -23,19 +28,25 @@ def lookup_json(json_file, args)
   n = m = 0
   data.each do |user|
     n += 1
+    # Assume match by default
     match = true
     filters.each do |keys, re|
+      # default mode for multi column keys is AND (means all columns must match regexp)
       mode = :all?
       index = 0
-      if keys.first.include?(':')
+      if keys.first[0] == ':'
+        # if we have ruby method (starts with :) then skip this key (index=1) and extract method name and convert to Symbol
         index = 1
         mode = keys.first[1..-1].to_sym
       end
       matches = []
+      # matches holds regexp match result for all keys
       keys.each_with_index do |key, i|
+        # skip ruby method name if needed and also value can be nil so value || ''
         next if i < index
         matches << (user[key] || '').match(re)
       end
+      # now we have array of match results and apply ruby method to it (default :all?) and get final match for all keys
       matched = matches.send(mode)
       unless matched
         match = false
@@ -62,4 +73,4 @@ if ARGV.size < 3
   exit(1)
 end
 
-lookup_json(ARGV[0], ARGV[1..-1])
+lookup_json(ARGV[0], ARGV[1..-2], ARGV[-1])
