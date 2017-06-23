d82a3616a8df7060ba283b4d201609bc62d21f47
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-23 10:04:41 +0200

    lukaszgryglicki-added-multi-column-keys-with-ruby-to_sym-metod-calls-like-any-all-etc

--- lookup_json.sh
+++ lookup_json.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.json
+ruby lookup_json.rb github_users.json ':any?, blog, location, bio' '/linkedin/i' affiliation '/^\(Unknown\)$/' unknown_with_linkedin.json
 ruby lookup_json.rb github_users.json blog '/[^\\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
