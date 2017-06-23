505cd5890e7fedf3895e877d9ec931073c483e92
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-23 09:39:21 +0200

    lukaszgryglicki-added-output-parameter-to-lookup-json-tool

--- lookup_json.sh
+++ lookup_json.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ruby lookup_json.rb github_users.json blog '/[^\\s]+/' affiliation '/^\(Unknown\)$/'
+ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.json
+ruby lookup_json.rb github_users.json blog '/[^\\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
