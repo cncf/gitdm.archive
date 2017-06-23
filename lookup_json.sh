7a9de75aa84444858898a4c8d641ddb06119ebe2
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-23 10:20:58 +0200

    lukaszgryglicki-more-description-to-lookup-json-tool

--- lookup_json.sh
+++ lookup_json.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# All unknown affiliations:
 ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.json
+# Devs with Unknown affiliation having case insensitive "linkedin" in their blog or bio or location:
 ruby lookup_json.rb github_users.json ':any?, blog, location, bio' '/linkedin/i' affiliation '/^\(Unknown\)$/' unknown_with_linkedin.json
+# Devs with Unknown affiliation having blog property non-empty
 ruby lookup_json.rb github_users.json blog '/[^\\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
