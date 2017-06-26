2754c942f2d9f70b7b191a5cb90a035337bb863e
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-26 10:49:42 +0200

    lukaszgryglicki-one-another-lookup-json-case

--- lookup_json.sh
+++ lookup_json.sh
@@ -5,3 +5,5 @@ ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.
 ruby lookup_json.rb github_users.json ':any?, blog, location, bio' '/linkedin/i' affiliation '/^\(Unknown\)$/' unknown_with_linkedin.json
 # Devs with Unknown affiliation having blog property non-empty
 ruby lookup_json.rb github_users.json blog '/[^\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
+# Devs with Unknown affiliation having location & name property non-empty
+ruby lookup_json.rb github_users.json 'location,name' '/[^\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_location_and_name.json
