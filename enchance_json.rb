1a6c46771efe72f6b7781f418b24e35472e63746
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-29 13:46:12 +0200

    lukaszgryglicki-still-need-to-process-some-special-emails

--- enchance_json.rb
+++ enchance_json.rb
@@ -7,12 +7,15 @@ def enchance_json(json_file, csv_file)
   # Process affiliations found by Python cncf/gitdm saved in CSV
   # "email","company","date_to"
   affs = {}
+  names = {}
   CSV.foreach(csv_file, headers: true) do |row|
     next if is_comment row
     h = row.to_h
     e = h['email'].strip
     c = h['company'].strip
+    n = h['name'].strip
     d = h['date_to'].strip
+    names[e] = n unless names.key?(e)
     affs[e] = [] unless affs.key?(e)
     if d && d.length > 0
       affs[e] << "#{c} < #{d}"
@@ -31,23 +34,38 @@ def enchance_json(json_file, csv_file)
 
   # Enchance JSON
   n_users = data.count
-  enchanced = not_found = 0
+  enchanced = csv_not_found = 0
   unks = []
+  json_emails = {}
   data.each do |user|
     e = user['email']
+    json_emails[e] = true
     v = '?'
     if affs.key?(e)
       enchanced += 1
       v = affs[e]
     else
-      not_found += 1
+      csv_not_found += 1
       unks << e
     end
     user['affiliation'] = v
   end
-  puts "Processed #{n_users} users, enchanced: #{enchanced}, not found: #{not_found}."
-  puts "Unknown emails (VIM search pattern):"
+
+  json_not_found = 0
+  unks2 = []
+  affs.each do |email, aff|
+    next unless aff == '(Unknown)'
+    unless json_emails.key?(email)
+      json_not_found += 1
+      unks2 << "#{email} #{names[email]}"
+    end
+  end
+  puts "Processed #{n_users} users, enchanced: #{enchanced}, not found in CSV: #{csv_not_found}, unknowns not found in JSON: #{json_not_found}."
+  puts "Unknown emails from JSON not found in CSV (VIM search pattern):"
   puts unks.join '\|'
+  puts "Unknown emails from CSV not found in JSON"
+  puts unks2.join("\n")
+  File.write 'not_found_in_json.txt', unks2.join("\n")
 
   # Write JSON back
   json = JSON.pretty_generate data
