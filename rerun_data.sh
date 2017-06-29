fb6b087f6afa7b547911ba640fa22d8f3e26364b
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-29 08:03:52 +0200

    luakszgryglicki-few-more-patterns-to-search-unknowns

--- rerun_data.sh
+++ rerun_data.sh
@@ -32,22 +32,5 @@ echo "New devs"
 echo "Multi repos"
 ./kubernetes_repos.sh
 
-echo "Generate all.txt file and all_affs.csv"
-./manual_all.sh
-
-echo "Correlations"
-./correlations.sh
-
-echo "Enchance JSON"
-./enchance_json.sh
-
-echo "Aliaser"
-./aliaser.sh
-
-echo "Lookup JSON"
-./lookup_json.sh
-
-echo 'Progress Report'
-./progress_report.sh
-
-echo 'Done'
+echo "Final analysis"
+./final_analysis.sh
