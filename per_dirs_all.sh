af49e396aca83a0dadde320ff0afce687f4314f8
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-07-05 12:17:25 +0200

    lukaszgryglicki-added-per-file-statistics-tool

---
+++ per_dirs_all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+PWD=`pwd`
+FN=$PWD/per_dirs/all
+LOG=$PWD/per_dirs/k8s_all.log
+cd ~/dev/go/src/k8s.io/kubernetes/
+git config merge.renameLimit 100000
+git config diff.renameLimit 100000
+echo 'Generating git log...'
+git log --numstat -M > $LOG
+ls -l $LOG
+echo 'Analysing data...'
+~/dev/cncf/gitdm/cncfdm.py -i $LOG -r '^vendor/|/vendor/' -R -d -n -b ~/dev/cncf/gitdm/ -u -I $FN.csv > $FN.out
+#~/dev/cncf/gitdm/cncfdm.py -i $LOG -r '^vendor/|/vendor/' -R -d -n -b ~/dev/cncf/gitdm/ -u -I $FN.csv
+git config --unset diff.renameLimit
+git config --unset merge.renameLimit
+cd $PWD
+ls -l $FN.csv
+echo 'Done.'
