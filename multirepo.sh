d219697f76200d59d0fab8d214220de8d8f74b7d
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-07-06 09:12:52 +0200

    lukaszgryglicki-skip-godeps

--- multirepo.sh
+++ multirepo.sh
@@ -18,9 +18,9 @@ do
 done
 PWD=$WD
 cd $PWD
-cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -u -h $F.html -o $F.txt -x $F.csv > $F.out
-cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -h $F2.html -o $F2.txt -x $F2.csv > $F2.out
-cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -m -h $F3.html -o $F3.txt -x $F3.csv > $F3.out
+cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/|^Godeps/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -u -h $F.html -o $F.txt -x $F.csv > $F.out
+cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/|^Godeps/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -h $F2.html -o $F2.txt -x $F2.csv > $F2.out
+cat $FN | ~/dev/cncf/gitdm/cncfdm.py -r '^vendor/|/vendor/|^Godeps/' -R -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -m -h $F3.html -o $F3.txt -x $F3.csv > $F3.out
 ./commits_in_default_ranges.sh all_kubernetes $FN
 #rm -f $FN
 #xz -9 $FN
