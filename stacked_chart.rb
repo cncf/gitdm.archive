5d1053e08be41dbda41937f914f38ff2244a8bcb
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-07-11 12:36:37 +0200

    lukaszgryglicki-added-stacked-chart-tool

---
+++ stacked_chart.rb
@@ -0,0 +1,103 @@
+require 'csv'
+require 'pry'
+require 'scanf'
+
+def map_name(name)
+  return '(Self)' if name == 'Self'
+  return '(Not Found)' if name == 'NotFound'
+  return name
+end
+
+def stacked_chart(files)
+  out = {}
+  files.each do |file|
+    d1 = Date.parse(file[-25..-16])
+    d2 = Date.parse(file[-14..-5])
+    days =  (d2 - d1).to_i
+    next unless days >= 28 && days <= 31
+    month = d1.to_s[0..-4]
+    mode = ''
+    File.readlines(file).each do |line|
+      line = line.strip
+      if line == ''
+        mode = ''
+        next
+      end
+      if line.downcase.include? 'top changeset contributors by employer'
+        mode = 'changeset'
+        next
+      elsif line.downcase.include? 'top lines changed by employer'
+        mode = 'lines'
+        next
+      elsif line.downcase.include? 'employers with the most signoffs'
+        mode = 'signoffs'
+        next
+      elsif line.downcase.include? 'employers with the most hackers'
+        mode = 'employers'
+        next
+      end
+      if mode == 'changeset'
+        data = line.split(' ')
+        data2 = data[-1]
+        data3 = data2.scanf('(%f%%)')
+        next if data2.length == 0
+        perc = data3[0]
+        n = data[-2].scanf("%d")[0]
+        next if n.to_s != data[-2]
+        name = data[0..-3].join(' ')
+        out[month] = {} unless out.key? month
+        if out[month].key? name
+          puts "WARNING: already have [#{month}][#{name}] = #{out[month][name]}, new value: #{[n, perc]} in file: #{file}" if out[month][name] != [n, perc]
+        else
+          name = map_name(name)
+          out[month][name] = [n, perc]
+        end
+      end
+    end
+  end
+  
+  companies = {}
+  n_months = out.keys.length.to_f
+  out.each do |month, data|
+    data.each do |company, values|
+      companies[company] = 0.0 unless companies.key?(company)
+      companies[company] += values[1] / n_months
+    end
+  end
+
+  companies.each do |company, csets|
+    companies[company] = csets.round(1)
+  end
+
+  ary = []
+  companies.each do |company, csets|
+    ary << [company, csets.round(1)]
+  end
+  ary = ary.sort_by { |row| -row[1] }[0...10].map { |row| row[0] }
+
+  hdr = ['Date'] + ary + ['Others']
+  CSV.open('stacked_chart.csv', "w", headers: hdr) do |csv|
+    csv << hdr
+    out.keys.sort.each do |month|
+      data = out[month]
+      sum_perc = 0.0
+      row = [month]
+      ary.each do |company|
+        values = data[company] || [0, 0]
+        perc = values[1]
+        sum_perc += perc
+        row << perc
+      end
+      row << (100.0 - sum_perc).round(1)
+      csv << row
+      p row
+    end
+  end
+end
+
+if ARGV.size < 1
+  puts "Missing arguments: list of test files"
+  exit(1)
+end
+
+stacked_chart(ARGV)
