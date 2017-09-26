1f3dc7bd36d7cb8e301b8e805f1cdebd7b0713da
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-26 15:04:03 +0000

    Update for 2017-09
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- update_all_repos.sh
+++ update_all_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/dev/go/src/k8s.io/
-for f in `find . -type d -depth 1`
+for f in `find . -type d -maxdepth 1 -iname "[a-zA-Z0-9]*"`
 do
   echo "$f"
   cd "$f" && git reset --hard && git checkout master && git pull
