f7ed13705dbe41c4d1d99ca610e0e48fd73a51e1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-04 06:47:20 +0000

    Add prometheus processing to ghusers, udpated affiliations
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- email_code.rb
+++ email_code.rb
@@ -1,8 +1,10 @@
 def email_decode(line)
+  return nil unless line
   line.gsub(/[^\s!]+![^\s!]+/) { |email| email.sub('!', '@') }
 end
 
 def email_encode(line)
+  return nil unless line
   line.gsub(/[^\s@]+@[^\s@]+/) { |email| email.sub('@', '!') }
 end
 
