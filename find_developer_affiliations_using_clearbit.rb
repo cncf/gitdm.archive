e49787a79473c55ce8c6a89cf93520ec5fd0329f
radoslaw <cabalrd@yahooo.com>
2017-12-02 00:05:52 +0100

    standardized code, find_dev...rb received human readable variable names
    Signed-off-by: radoslaw <cabalrd@yahooo.com>

--- find_developer_affiliations_using_clearbit.rb
+++ find_developer_affiliations_using_clearbit.rb
@@ -6,87 +6,88 @@ require 'Clearbit'
 require 'json'
 #Clearbit.key = ENV['CLEARBIT_KEY']
 # !!! !!! !!! ask Rad at cabalrd@yahoo.com for an API key under a user set up for enrichment subscription !!! !!! !!!
-line_num = 0
+Clearbit.key = 'sk_ab962f5c253faf729edffbe5ec28c23e'
+line_count = 0
 start_found = false
-em_li = []
+email_list = []
 text = File.open('all.txt').read
-text.gsub!(/\r\n?/, "\n")
+text.gsub!(/\r\n?/, '\n')
 text.each_line do |line|
-  #sl = line.scan(/\d+/)
-  sl = line.split
-  if sl[0] == 'Developers'
+  #line_word_array = line.scan(/\d+/)
+  line_word_array = line.split
+  if line_word_array[0] == 'Developers'
     start_found = true
     next
   end
   if start_found
-    if sl[0] == '(Unknown)' || sl[0] == 'NotFound'
-      em_li.push sl[1]
-      line_num += 1
+    if line_word_array[0] == '(Unknown)' || line_word_array[0] == 'NotFound'
+      email_list.push line_word_array[1]
+      line_count += 1
     else
       break
     end
   end
-  #em_li.sort!
+  #email_list.sort!
 end
 
-print "line count #{line_num}\n"
+print "line count: #{line_count}\n"
 
-#puts em_li.inspect
-chk_cnt = 1
+#puts email_list.inspect
+check_cnt = 1
 
 ok_cnt = bad_cnt = err_cnt = 0
 
-CSV.open('developer_affiliation_lookup.csv', 'w') do |csv|
-  csv << ["email","chance","affiliation_suggestion","hashed_email","first_name", "last_name", "full_name", "gender", "localization", "bio", "site", "avatar", "employment_name", "employment_domain", "github_handle", "github_company", "github_blog", "linkedin_handle", "googleplus_handle", "aboutme_handle", "gravatar_handle", "aboutme_bio" ]
-  em_li.each do |ae|
-    if chk_cnt <= 1234 #    !!!!!     THIS is the max NUMBER of emails to PROCESS in this BATCH    !!!!!
-      em_il = ae.sub('!','@')
+CSV.open('developer_affiliation_lookup_test.csv', 'w') do |csv|
+  csv << ['email','chance','affiliation_suggestion','hashed_email','first_name', 'last_name', 'full_name', 'gender', 'localization', 'bio', 'site', 'avatar', 'employment_name', 'employment_domain', 'github_handle', 'github_company', 'github_blog', 'linkedin_handle', 'googleplus_handle', 'aboutme_handle', 'gravatar_handle', 'aboutme_bio' ]
+  email_list.each do |email_with_at|
+    if check_cnt <= 7 #    !!!!!     THIS is the max NUMBER of emails to PROCESS in this BATCH    !!!!!
+      email_with_exclamation = email_with_at.sub('!','@')
       begin
-        result = Clearbit::Enrichment.find(email: em_il, stream: true)
-          p = result.person
-          suggestion = ''
-          c = "none"
-          pfn = "#{p&.name&.given_name}".downcase
-          pln = "#{p&.name&.family_name}".downcase
-          #binding.pry
-          if !p&.employment&.name.nil? && (p.employment.name == "#{pfn}#{pln}" || p.employment.name == "#{pfn} #{pln}")
-            r = "Self"
-            chance = "none"
-          end
-          if !p&.employment&.name.nil? && (p.employment.name.downcase.include? "university") && (p.employment.name.downcase.include? "institute") && (p.employment.name.downcase.include? "academy") && !p&.github&.company.nil? && p.github.company != ""
-            r = p.github.company
-            chance = "low"
-          end
-          if !p&.github&.company.nil? && p.github.company != ""
-            r = p.github.company
-            chance = "mid"
-          end
-          if !p&.employment&.name.nil? && p.employment.name != "" && p.employment.name != "GitHub"
-            r = p.employment.name
-            chance = "high"
-          end          
-          suggestion = r
-          csv << ["#{p.email}","#{chance}","#{suggestion}","#{ae}","#{p.name.given_name}","#{p.name.family_name}","#{p.name.fullName}","#{p.gender}","#{p.location}","#{p.bio}","#{p.site}","#{p.avatar}","#{p.employment.name}","#{p.employment.domain}","#{p.github.handle}","#{p.github.company}","#{p.github.blog}","#{p.linkedin.handle}","#{p.googleplus.handle}","#{p.aboutme.handle}","#{p.gravatar.handle}","#{p.aboutme.bio}"]
-          ok_cnt += 1
-          puts "#{chk_cnt} got an enrichment"
-        rescue StandardError => bang
+        result = Clearbit::Enrichment.find(email: email_with_exclamation, stream: true)
+        person = result.person
+        suggestion = ''
+        chance = ''
+        first_name = "#{person&.name&.given_name}".downcase
+        last_name = "#{person&.name&.family_name}".downcase
+        #binding.pry
+        if !person&.employment&.name.nil? && (person.employment.name == "#{first_name}#{last_name}" || person.employment.name == "#{first_name} #{last_name}")
+          temp_suggestion = 'Self'
+          chance = 'none'
+        end
+        if !person&.employment&.name.nil? && (person.employment.name.downcase.include? 'university') && (person.employment.name.downcase.include? 'institute') && (person.employment.name.downcase.include? 'academy') && !person&.github&.company.nil? && person.github.company != ''
+          temp_suggestion = person.github.company
+          chance = 'low'
+        end
+        if !person&.github&.company.nil? && person.github.company != ''
+          temp_suggestion = person.github.company
+          chance = 'mid'
+        end
+        if !person&.employment&.name.nil? && person.employment.name != '' && person.employment.name != 'GitHub'
+          temp_suggestion = person.employment.name
+          chance = 'high'
+        end          
+        suggestion = temp_suggestion
+        csv << ["#{person.email}","#{chance}","#{suggestion}",email_with_at,"#{person.name.given_name}","#{person.name.family_name}","#{person.name.fullName}","#{person.gender}","#{person.location}","#{person.bio}","#{person.site}","#{person.avatar}","#{person.employment.name}","#{person.employment.domain}","#{person.github.handle}","#{person.github.company}","#{person.github.blog}","#{person.linkedin.handle}","#{person.googleplus.handle}","#{person.aboutme.handle}","#{person.gravatar.handle}","#{person.aboutme.bio}"]
+        ok_cnt += 1
+        puts "#{check_cnt} got an enrichment"
+      rescue StandardError => bang
         hash = JSON[bang]
         hash = JSON.parse(hash)
-        if hash.index("email_invalid")
-          csv << ["#{ae}","none","","error","invalid", "email", "address"]
+        if hash.index('email_invalid')
+          csv << [email_with_at,'none','','error','invalid', 'email', 'address']
           bad_cnt += 1
-          puts "#{chk_cnt} received a bad email msg"
+          puts "#{check_cnt} received a bad email msg"
         else
-          csv << ["#{ae}","none","","error","bad", "response"]
-          puts "#{chk_cnt} #{bang}"
+          csv << [email_with_at,'none','','error','bad', 'response']
+          puts "#{check_cnt} #{bang}"
           err_cnt += 1
         end
       end
-    chk_cnt += 1
+      check_cnt += 1
     end
   end
 end
-puts "done processing with Clearbit"
+puts 'done processing with Clearbit'
 puts "count of found: #{ok_cnt}"
 puts "count of bad emails: #{bad_cnt}"
 puts "count of errored-out: #{err_cnt}"
--- merge_clearbit_affiliations_into_map.rb
+++ merge_clearbit_affiliations_into_map.rb
@@ -1,7 +1,7 @@
 #read developer_affiliation_lookup.csv
 #read email-map
 #remove company suffixes such as Inc.,
-#Everything with "learning", "university", in its name is suspected to be Self. maybe also "institute", "Software Engineer "
+#Everything with 'learning', 'university', in its name is suspected to be Self. maybe also 'institute', 'Software Engineer '
 #Samsung SDS is separate from other Samsung 
 #Samsund Co., Samsung corp., Samsung Electronics, Samsung Mobile etc. They're all just Samsung.
 #Also normalize samsung, Samsung, SAMSUNG etc.
@@ -16,177 +16,177 @@ require './comment'
 
 line_num = 0
 map_list = []
-puts "reading the email-map file"
+puts 'reading the email-map file'
 text = File.open('cncf-config/email-map').read
-text.gsub!(/\r\n?/, "\n")
+text.gsub!(/\r\n?/, '\n')
 text.each_line do |line|
-	sl = line.gsub(/\s+/m, ' ').strip.split(" ")
-	if sl[0] != '#'
-	  map_list.push sl
-	  line_num += 1
-	end
+  sl = line.gsub(/\s+/m, ' ').strip.split(' ')
+  if sl[0] != '#'
+    map_list.push sl
+    line_num += 1
+  end
 end
 puts "found #{line_num} mappings in email-map file"
 
 def correct_company_name(a_s)
-	#puts "received #{a_s}"
-	#remove suffixes like: Co., Ltd., Corp., Inc., Limited., LLC, Group. from company names.
-	ra = [" GmbH & Co."," S.A."," Co.,"," Co."," Co"," Corp.,"," Corp."," Corp"," GmbH.,"," GmbH."," GmbH"," Group.,"," Group."," Group"," Inc.,", " Inc."," Inc"," Limited.,"," Limited."," Limited"," LLC.,"," LLC."," LLC"," Ltd.,"," Ltd."," Ltd"," PLC"," S.à r.L."]
-	ra.each do |sg|
-		a_s.sub!(sg,"")
-	end
-	a_s.sub!(/^@/, "")    #remove begigging @
-	a_s.sub!(/.com$/, "") #remove ending .com
-	#puts "returned #{a_s}"
-	#binding.pry
+    #puts "received #{a_s}"
+    #remove suffixes like: Co., Ltd., Corp., Inc., Limited., LLC, Group. from company names.
+    ra = [' GmbH & Co.',' S.A.',' Co.,',' Co.',' Co',' Corp.,',' Corp.',' Corp',' GmbH.,',' GmbH.',' GmbH',' Group.,',' Group.',' Group',' Inc.,', ' Inc.',' Inc',' Limited.,',' Limited.',' Limited',' LLC.,',' LLC.',' LLC',' Ltd.,',' Ltd.',' Ltd',' PLC',' S.à r.L.']
+    ra.each do |sg|
+      a_s.sub!(sg,'')
+    end
+    a_s.sub!(/^@/, '')    #remove begigging @
+    a_s.sub!(/.com$/, '') #remove ending .com
+    #puts "returned #{a_s}"
+    #binding.pry
     return a_s
-end
+  end
 
-def check_for_self_employment(a_s)
-	cn = a_s&.downcase
-	selfies = ["learning","university","institute","school","software engineer","self-employed","self employed","evangelist","enthusiast","self"]
-	selfies.each do |selfie|	
-		if cn.include? selfie
-		   a_s = "Self"
-		end
-	end
-	return a_s
-end
+  def check_for_self_employment(a_s)
+    cn = a_s&.downcase
+    selfies = ['learning','university','institute','school','software engineer','self-employed','self employed','evangelist','enthusiast','self']
+    selfies.each do |selfie|    
+      if cn.include? selfie
+       a_s = 'Self'
+     end
+   end
+   return a_s
+ end
 
-def normalize_samsung(a_s)
-	#Samsung SDS is separate from other Samsung 
-	#Samsundg Co., Samsung corp., Samsung Electronics, Samsung Mobile etc. They're all just Samsung, proper case
-	cn = a_s&.downcase
-	if cn&.include? "samsung"
-		a_s.gsub(/samsung/i, 'Samsung')
-	end
-	if ['samsung electronics','samsung mobile'].include? "#{cn}"
-		a_s = 'Samsung'
-	end
-	return a_s
-end
-
-def normalize_hewlettpackard(a_s)
-	#HP and Hewlett-Packard to HPE
-	cn = a_s&.downcase
-	if ['hewlett-packard','hewlettpackard','hewlett packard','hp'].include? "#{cn}"
-		a_s = 'HPE'
-	end
-	return a_s
-end
-
-def normalize_amazonwebservices(a_s)
-	#change Amazon Web Services to AWS
-	cn = a_s&.downcase
-	if cn&.include? "amazon web services"
-		a_s = "AWS"
-	end
-	return a_s
-end
-
-def normalize_soundcloud(a_s)
-	#change SoundCloud … to SoundCloud
-	cn = a_s&.downcase
-	if cn&.include? "soundcloud "
-		a_s = "SoundCloud"
-	end
-	return a_s
-end
-
-def normalize_ghostcloud(a_s)
-	#change GhostCloud … to SoundCloud
-	cn = a_s&.downcase
-	if cn&.include? "ghostcloud "
-		a_s = "CNCF"
-	end
-	return a_s
-end
-
-def normalize_possessive(a_s)
-	#remove ' if company ends with '
-	a_s&.sub!(/'$/, "")
+ def normalize_samsung(a_s)
+    #Samsung SDS is separate from other Samsung 
+    #Samsundg Co., Samsung corp., Samsung Electronics, Samsung Mobile etc. They're all just Samsung, proper case
+    cn = a_s&.downcase
+    if cn&.include? 'samsung'
+      a_s.gsub(/samsung/i, 'Samsung')
+    end
+    if ['samsung electronics','samsung mobile'].include? "#{cn}"
+      a_s = 'Samsung'
+    end
     return a_s
-end
+  end
 
-suggestions = []
-CSV.foreach('developer_affiliation_lookup.csv', headers: true) do |row|
-	next if is_comment row
-	h = row.to_h
-	a_s = h['affiliation_suggestion']
-	# add emails with no company as NotFound
-	# if company is name associated with email, do Self
-	# base on columns: chance, affiliation_suggestion, hashed_email
-	if ['high','mid','low'].include? h['chance']
-		a_s = correct_company_name(a_s)
-		a_s = check_for_self_employment(a_s)
-		a_s = normalize_samsung(a_s)
-		a_s = normalize_hewlettpackard(a_s)
-		a_s = normalize_amazonwebservices(a_s)
-		a_s = normalize_soundcloud(a_s)
-		a_s = normalize_ghostcloud(a_s)
-		a_s = normalize_possessive(a_s)
-		suggestion = [ h['hashed_email'], a_s ]
-		#binding.pry
-		suggestions.push suggestion
-	else #add Unknowns
-		suggestion = [ h['hashed_email'], "NotFound" ]
-		suggestions.push suggestion
-	end
-end
-puts "found #{suggestions.size} suggestions in developer_affiliation_lookup.csv file"
+  def normalize_hewlettpackard(a_s)
+    #HP and Hewlett-Packard to HPE
+    cn = a_s&.downcase
+    if ['hewlett-packard','hewlettpackard','hewlett packard','hp'].include? "#{cn}"
+      a_s = 'HPE'
+    end
+    return a_s
+  end
+
+  def normalize_amazonwebservices(a_s)
+    #change Amazon Web Services to AWS
+    cn = a_s&.downcase
+    if cn&.include? 'amazon web services'
+      a_s = 'AWS'
+    end
+    return a_s
+  end
+
+  def normalize_soundcloud(a_s)
+    #change SoundCloud … to SoundCloud
+    cn = a_s&.downcase
+    if cn&.include? 'soundcloud '
+      a_s = 'SoundCloud'
+    end
+    return a_s
+  end
+
+  def normalize_ghostcloud(a_s)
+    #change GhostCloud … to GhostCloud
+    cn = a_s&.downcase
+    if cn&.include? 'ghostcloud '
+      a_s = 'Ghostcloud'
+    end
+    return a_s
+  end
+
+  def normalize_possessive(a_s)
+    #remove ' if company ends with '
+    a_s&.sub!(/'$/, '')
+    return a_s
+  end
+
+  suggestions = []
+  CSV.foreach('developer_affiliation_lookup.csv', headers: true) do |row|
+    next if is_comment row
+    h = row.to_h
+    a_s = h['affiliation_suggestion']
+    # add emails with no company as NotFound
+    # if company is name associated with email, do Self
+    # base on columns: chance, affiliation_suggestion, hashed_email
+    if ['high','mid','low'].include? h['chance']
+      a_s = correct_company_name(a_s)
+      a_s = check_for_self_employment(a_s)
+      a_s = normalize_samsung(a_s)
+      a_s = normalize_hewlettpackard(a_s)
+      a_s = normalize_amazonwebservices(a_s)
+      a_s = normalize_soundcloud(a_s)
+      a_s = normalize_ghostcloud(a_s)
+      a_s = normalize_possessive(a_s)
+      suggestion = [ h['hashed_email'], a_s ]
+        #binding.pry
+        suggestions.push suggestion
+    else #add Unknowns
+      suggestion = [ h['hashed_email'], 'NotFound' ]
+      suggestions.push suggestion
+    end
+  end
+  puts "found #{suggestions.size} suggestions in developer_affiliation_lookup.csv file"
 
 #now check for existence to decide on update or insertion
 ar = ur = 0
 text = File.read('cncf-config/email-map')
 suggestions.each do |sg|
-	#sg[1] can be a company name or Self or NotFound
+    #sg[1] can be a company name or Self or NotFound
 
-	#if email found and has something other than self or notfound but new data has something, overwrite
-	#if data record is new, add
+    #if email found and has something other than self or notfound but new data has something, overwrite
+    #if data record is new, add
 
-	ech = "#{sg[0]} #{sg[1]}"
-	ect = "#{ech}\n" #new entry based on Clearbit
+    ech = "#{sg[0]} #{sg[1]}"
+    ect = "#{ech}\n" #new entry based on Clearbit
 
-	if !['Self', 'NotFound'].include? sg[1]
-		if !text.include? ect
-			# append to end if the email does not already have a company assigment
-			short_list = []
-			map_list.each do |ml|
-				if ml[0] == sg[0]
-					short_list.push "#{ml[0]} #{ml[1]}"
-				end
-			end
-			if !short_list.include? ech
-				text << ect
-				ar += 1
-			end
-		end
-	else
-		if (!['Self', 'NotFound'].include? "#{sg[1]}") && (text.include? "#{sg[0]} Self")
-			# replace existing Self with a company
-			text = text.gsub(/#{sg[0]} Self/, "#{ect}")
-			ur += 1
-		elsif "#{sg[1]}" == 'Self' && (text.include? "#{sg[0]} NotFound")
-			# replace existing NotFound with Self
-			text = text.gsub(/#{sg[0]} NotFound/, "#{ect}")
-			ur += 1
-		end
-	end
+    if !['Self', 'NotFound'].include? sg[1]
+      if !text.include? ect
+            # append to end if the email does not already have a company assigment
+            short_list = []
+            map_list.each do |ml|
+              if ml[0] == sg[0]
+                short_list.push "#{ml[0]} #{ml[1]}"
+              end
+            end
+            if !short_list.include? ech
+              text << ect
+              ar += 1
+            end
+          end
+        else
+          if (!['Self', 'NotFound'].include? "#{sg[1]}") && (text.include? "#{sg[0]} Self")
+            # replace existing Self with a company
+            text = text.gsub(/#{sg[0]} Self/, ect)
+            ur += 1
+          elsif sg[1] == 'Self' && (text.include? "#{sg[0]} NotFound")
+            # replace existing NotFound with Self
+            text = text.gsub(/#{sg[0]} NotFound/, ect)
+            ur += 1
+          end
+        end
 
-end
+      end
 
 # Write changes back to the file
-File.open('cncf-config/email-map', "w") {|file| file.puts text }
+File.open('cncf-config/email-map', 'w') {|file| file.puts text }
 
-puts "altered the email-map file with Clearbit suggestions"
+puts 'altered the email-map file with Clearbit suggestions'
 puts "updated #{ur} records}"
 puts "added #{ar} records}"
 
 new_array = File.readlines('cncf-config/email-map').sort
-File.open('cncf-config/email-map',"w") do |file|
+File.open('cncf-config/email-map','w') do |file|
   file.puts new_array
 end
 
-puts "sorted email-map"
+puts 'sorted email-map'
 
-puts "all done"
+puts 'all done'
