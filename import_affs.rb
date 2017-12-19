e87dc0c88cdf21932f977cdc096d3d447fada5a2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-19 10:29:21 +0000

    Rename Self -> Independent - regenerated data, fixed scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- import_affs.rb
+++ import_affs.rb
@@ -162,7 +162,7 @@ def import_affs(dev_affs, comp_devs)
     puts 'Config files were out of sync, to sync them again:'
     puts 'mv email-map cncf-config/email-map'
     puts './multirepo.sh ~/dev/kubernetes_repos/* or ./all_repos_log.sh ~/dev/kubernetes_repos/* ~/dev/prometheus/*'
-    puts './manual_all.sh or `~/dev/cncf/gitdm/cncfdm.py -i git.log -r "^vendor/|/vendor/|^Godeps/" -R -n -b ./ -t -z -d -D -U -u -o all.txt -x all.csv -a all_affs.csv > all.out`'
+    puts './manual_all.sh or `~/dev/alt/gitdm/cncfdm.py -i git.log -r "^vendor/|/vendor/|^Godeps/" -R -n -b ./ -t -z -d -D -U -u -o all.txt -x all.csv -a all_affs.csv > all.out`'
     puts 'SKIP_COMPANIES="(Unknown)" ./gen_aff_files.sh'
     puts 'Consider ./ghusers.sh'
     puts './enchance_json.sh'
