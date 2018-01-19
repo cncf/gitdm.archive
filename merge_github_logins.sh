684881a0aa7e67d7ddf138b31a735a7230ef1f75
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-19 09:53:04 +0000

    Fixed duplicates in 'cncf-config/email-map'
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- merge_github_logins.sh
+++ merge_github_logins.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ruby merge_github_logins.rb 'github_users.json' >> cncf-config/email-map
 ./sort_configs.sh
+ruby check_map_file.rb cncf-config/email-map > out
+mv out cncf-config/email-map
