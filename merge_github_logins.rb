d3e1b2c077ac1e5cecc6518137d875b58b891586
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-27 21:17:54 +0000

    Maybe fixed multiple affiliations issue
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- merge_github_logins.rb
+++ merge_github_logins.rb
@@ -1,55 +1,13 @@
 require 'pry'
-require 'json'
+require './merge'
 
 def merge_github_logins(json_file)
-  profs = {}
+  # Parse input JSON
   data = JSON.parse File.read json_file
-  data.each_with_index do |user, i|
-    login = user['login']
-    profs[login] = [] unless profs.key?(login)
-    profs[login] << [user, i]
-  end
-  mp = {}
-  profs.each do |login, profiles|
-    mp[login] = profiles if profiles.length > 1
-  end
-  profs = nil
-  mp.each do |login, profiles|
-    unknowns = []
-    knowns = []
-    profiles.each do |profile_data|
-      profile, i = *profile_data
-      affiliation = profile['affiliation']
-      if ['?', 'NotFound', '(Unknown)'].include?(affiliation)
-        unknowns << [profile, i]
-      else
-        knowns << [profile, i]
-      end
-    end
-    if unknowns.length > 0 and knowns.length > 0
-      aff = knowns.first[0]['affiliation']
-      conflict = false
-      knowns.each do |profile_data|
-        profile, i = *profile_data
-        curr_aff = profile['affiliation']
-        unless curr_aff == aff
-          email = knowns.first[0]['email']
-          curr_email = profile['email']
-          STDERR.puts "Affiliations conflict: login: #{login} #{email}:#{aff} != #{curr_email}:#{curr_aff}"
-          conflict = true
-        end
-      end
-      next if conflict
-      unknowns.each do |profile_data|
-        profile, i = *profile_data
-        data[i]['affiliation'] = aff
-        email = profile['email']
-        aff.split(', ').each do |aff_line|
-          puts "#{email} #{aff_line}"
-        end
-      end
-    end
-  end
+
+  # Merge multiple logins
+  merge_multiple_logins data, true
+
   # Write JSON back
   json = JSON.pretty_generate data
   File.write json_file, json
