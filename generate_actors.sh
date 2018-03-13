b495426233777881e21d4909e96d9cd5665ccfe4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-13 07:03:26 +0000

    New fully automatic 'generate_actors.sh'
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- generate_actors.sh
+++ generate_actors.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
-sudo -u postgres psql gha < ~/dev/go/src/devstats/util_sql/contributing_actors.sql > actors.txt
-sudo -u postgres psql prometheus < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql opentracing < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql fluentd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql linkerd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql grpc < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql coredns < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql containerd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql rkt < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql cni < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql envoy < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql jaeger < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql notary < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql tuf < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql rook < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql vitess < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql opencontainers < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-sudo -u postgres psql cncf < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
-vim actors.txt
+sudo -u postgres psql -tA gha < ~/dev/go/src/devstats/util_sql/contributing_actors.sql > actors.txt
+sudo -u postgres psql -tA prometheus < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA opentracing < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA fluentd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA linkerd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA grpc < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA coredns < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA containerd < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA rkt < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA cni < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA envoy < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA jaeger < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA notary < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA tuf < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA rook < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA vitess < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA nats < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA opencontainers < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+sudo -u postgres psql -tA cncf < ~/dev/go/src/devstats/util_sql/contributing_actors.sql >> actors.txt
+cat actors.txt | sort | uniq > actors.tmp
+tr '\n' ',' < actors.tmp > out
+rm actors.tmp
+mv out actors.txt
+truncate -s-1 actors.txt
