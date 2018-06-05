0f2f5132846117b7e64d04ad48c68d694ccf8298
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-06-05 09:30:58 +0000

    Stackalytics import wip - cncf-config/email-map part
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- import_from_stackalytics.rb
+++ import_from_stackalytics.rb
@@ -14,9 +14,40 @@ File.readlines('cncf-config/email-map').each do |line|
   email = email_encode(arr[0])
   company = arr[1..-1].join ' '
   didx = company.index(' < ')
-  company = company[0..didx - 1] if didx
+  dtto = nil
+  comp = company
+  if didx
+    comp = company[0..didx - 1]
+    dtto = company[didx + 3..-1]
+  end
   next if company.strip == 'NotFound' || company.strip[0..11] == 'Independent'
-  existing[email] = company
+  existing[email] = {} unless existing.key?(email)
+  existing[email][comp] = dtto
 end
 
+data = JSON.parse File.read 'default_data.json'
+
+conf = 0
+new = 0
+same = 0
+
+data['users'].each do |user|
+  user['emails'].each do |email|
+    email = email_encode email
+    if existing.key?(email)
+      user['companies'].each do |company|
+        if existing[email][company['company_name']] != company['end_date']
+          p [email, company, existing[email], existing[email][company['company_name']], company['end_date']]
+          conf += 1
+        else
+          same += 1
+        end
+      end
+    else
+      new += 1
+    end
+  end
+end
+
+puts "Same: #{same}, new: #{new}, conflict: #{conf}"
 binding.pry
