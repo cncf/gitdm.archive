c864ca8a99434ac9a78bb544e1b333d4baf8ab19
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-06-06 12:03:53 +0000

    Import from stackalytics phase 1 probably finished
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- import_from_stackalytics.sh
+++ import_from_stackalytics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#rm default_data.json || exit 1
-#wget https://raw.githubusercontent.com/openstack/stackalytics/master/etc/default_data.json || exit 2
+rm default_data.json || exit 1
+wget https://raw.githubusercontent.com/openstack/stackalytics/master/etc/default_data.json || exit 2
 ./import_from_stackalytics.rb || exit 3
 cat email-map | sort | uniq > cncf-config/email-map || exit 4
 ./sort_configs.sh
