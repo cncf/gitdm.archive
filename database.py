c6b5b6ac24b2946a6f2628ae55e99456e4d40449
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-13 07:41:14 +0000

    report unknowns separately to 'unknowns.txt'
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- database.py
+++ database.py
@@ -36,6 +36,9 @@ class Hacker:
     def full_name_with_aff(self):
         return self.employer[0][0][1].name + ' ' + self.email[0] + ' ' + self.name
 
+    def full_name_with_aff_tabs(self):
+        return self.employer[0][0][1].name + '\t' + self.email[0] + '\t' + self.name
+
     def repr(self):
         return ('Hacker', self.id, self.name, 'Email', self.email[0], 'Employers', len(self.employer))
 
