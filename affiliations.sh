bb8debc9611ebd554558d8fd59cc16e7c55399c0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-05 10:27:26 +0000

    Added final import affiliations script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- affiliations.sh
+++ affiliations.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ruby affiliations.rb affiliations.csv
+ruby affiliations.rb affiliations.csv > affiliations.out
+cat affiliations.out
+echo "Affiliations saved in affiliations.out, you can add them to email-map via 'cat affiliations.out >> cncf-config/email-map'"
