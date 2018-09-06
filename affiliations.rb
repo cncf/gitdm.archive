6a715d2597f858c7622ba7201371421030f3d7fd
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-06 12:42:35 +0000

    Update affiliations checker
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- affiliations.rb
+++ affiliations.rb
@@ -5,13 +5,16 @@ require './email_code'
 
 def affiliations(affiliations_file)
   all_affs = []
+  ln = 1
   CSV.foreach(affiliations_file, headers: true) do |row|
+    ln += 1
     next if is_comment row
     h = row.to_h
+    h['line_no'] = ln
     possible_emails = (h['new emails'] || '').split(',').map(&:strip) << h['email'].strip
     emails = ((h['new emails'] || '').split(',').map(&:strip).map { |e| email_encode(e) } << email_encode(h['email'].strip)).reject { |e| e.nil? || e.empty? || !e.include?('!') }.uniq
     if emails.length != possible_emails.length
-      puts "Wrong emails config"
+      puts "Wrong emails config (some discarded)"
       p h
       binding.pry
       next
@@ -19,16 +22,39 @@ def affiliations(affiliations_file)
     possible_affs = (h['affiliations'] || '').split(',').map(&:strip)
     affs = possible_affs.reject { |a| a.nil? || a.empty? || a == '/' }.uniq
     if affs.length != possible_affs.length
-      puts "Wrong affiliations config"
+      puts "Wrong affiliations config (some discarded)"
       p h
       binding.pry
       next
     end
+    n_final = 0
     affs.each do |aff|
+      ary = aff.split('<').map(&:strip)
+      n_final += 1 if ary.length == 1
+    end
+    if n_final != 1
+      puts "Wrong affiliation config - there must be exactly one final affiliation"
+      p affs
+      p h
+      binding.pry
+      next
+    end
+
+    affs.each do |aff|
+      begin
+        ddt = DateTime.strptime(aff, '%Y-%m-%d')
+        sdt = ddt.strftime("%Y-%m-%d")
+        puts "Wrong affiliation config - YYYY-MM-DD date found where company name expected"
+        p aff
+        p h
+        binding.pry
+        next
+      rescue
+      end
       possible_data = aff.split('<').map(&:strip)
       data = possible_data.reject { |a| a.nil? || a.empty? }.uniq
       if data.length < 1 || data.length > 2 || data.length != possible_data.length
-        puts "Wrong affiliation config"
+        puts "Wrong affiliation config (multiple < or empty discarded values)"
         p data
         p h
         binding.pry
@@ -50,10 +76,11 @@ def affiliations(affiliations_file)
           sdt = ddt.strftime("%Y-%m-%d")
           com = data[0]
           emails.each { |e| all_affs << "#{e} #{com} < #{sdt}" }
-        rescue => err2
+        rescue => err
           puts "Wrong date format expected YYYY-MM-DD, got #{dt} (invalid date)"
           p data
           p h
+          p err
           binding.pry
           next
         end
