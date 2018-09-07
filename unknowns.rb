c17489a4585a9670c8ff0446993d56ad6a7a0368
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-07 12:12:29 +0000

    Added default LinkedIn search link
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- unknowns.rb
+++ unknowns.rb
@@ -30,12 +30,16 @@ end
 
 f = nf = 0
 email2line.each do |email, line|
+  ary = line.split "\t"
+  name = ary[2]
+  escaped_name = URI.escape(name)
+  search = "https://www.linkedin.com/search/results/index/?keywords=#{escaped_name}"
   if email2gh.key?(email)
     logins = email2gh[email]
-    email2line[email] = "#{line}\t#{logins.join(',')}"
+    email2line[email] = "#{line}\t#{logins.join(',')}\t#{search}"
     f += 1
   else
-    email2line[email] = "#{line}\t-"
+    email2line[email] = "#{line}\t-\t#{search}"
     nf += 1
   end
 end
@@ -46,11 +50,11 @@ arr = []
 email2line.each { |email, line| arr << line.split("\t") }
 arr = arr.sort_by { |item| [item[0], -item[3].to_i] }
 
-hdr = %w(type email name github patches)
+hdr = %w(type email name github search patches)
 CSV.open('unknowns.csv', 'w', headers: hdr) do |csv|
   csv << hdr
   arr.each do |ary|
-    csv << [ary[0], ary[1], ary[2], ary[4], ary[3]]
+    csv << [ary[0], ary[1], ary[2], ary[4], ary[5], ary[3]]
   end
 end
 
