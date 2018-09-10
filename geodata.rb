a54d6399b0eb30cfb495c070f35afa7b9ec60441
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-10 09:45:47 +0000

    Final generate database
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- geodata.rb
+++ geodata.rb
@@ -3,9 +3,12 @@ require 'pg'
 
 def geodata(geodata_file)
   c = PG.connect( host: 'localhost', dbname: 'geonames', user: 'gha_admin', password: ENV['PG_PASS'] )
+  # set skip_conflict to true to skip insert conflicts, this should not be needed on a clean database
+  skip_conflict = false
   cols = 19
   rows = 0
   altnames = []
+  geodata = []
   File.readlines(geodata_file).each do |row|
     vals = row.split "\t"
     if vals.count != cols
@@ -28,11 +31,13 @@ def geodata(geodata_file)
       vals[14].to_i,
       vals[17]
     ]
+    geodata << ary if ary.length > 0
     ary2 = vals[3].split(',').map(&:strip).reject(&:nil?)
-    if ary2.length > 0
-      altnames << [gnid, vals[3].split(',').map(&:strip).reject(&:nil?)]
-    end
+    altnames << [gnid, vals[3].split(',').map(&:strip).reject(&:nil?)] if ary2.length > 0
+    puts "Row #{rows}" if rows % 10000 == 0
   end
+  # alternate names
+  puts "Rows #{rows}\nMass inserting altername names..."
   q = "insert into alternatenames(geonameid, altname) values "
   n = 0
   vars = []
@@ -46,10 +51,23 @@ def geodata(geodata_file)
     end
   end
   q = q[0..(q.length-3)] if n > 0
-  q = q + " on conflict do nothing"
+  q = q + " on conflict do nothing" if skip_conflict
   c.prepare('alternatenames_q', q) 
   c.exec_prepared('alternatenames_q', vars)
-  binding.pry
+  # geodata
+  puts "Mass inserting geonames"
+  q = "insert into geonames(geonameid, name, asciiname, latitude, longitude, countrycode, ac1, ac2, ac3, ac4, population, tz) values "
+  n = 0
+  vars = []
+  geodata.each do |row|
+    q += "($#{n+1}, $#{n+2}, $#{n+3}, $#{n+4}, $#{n+5}, $#{n+6}, $#{n+7}, $#{n+8}, $#{n+9}, $#{n+10}, $#{n+11}, $#{n+12}), "
+    n += 12
+    row.each { |col| vars << col }
+  end
+  q = q[0..(q.length-3)] if n > 0
+  q = q + " on conflict do nothing" if skip_conflict
+  c.prepare('geodata_q', q)
+  c.exec_prepared('geodata_q', vars)
 end
 
 if ARGV.size < 1
