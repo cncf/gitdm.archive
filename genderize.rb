8fc459ae89c503a5a7009e69d8d3229e4ae9a35d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-11 09:21:01 +0000

    More genderize work
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- genderize.rb
+++ genderize.rb
@@ -2,13 +2,44 @@ require 'net/http'
 require 'json'
 require 'uri'
 require 'pry'
+require 'unidecoder'
+
+$gcache = {}
+$hit = 0
+$miss = 0
 
 def get_sex(name, login, cid)
-  #uri = URI.parse("https://api.genderize.io?apikey=8d41a90f2dc1f41ecbf5d145ac1c47aa&name=kim&country_id=dk")
-  #response = Net::HTTP.get_response(uri)
-  #data = JSON.parse(response.body)
-  #p data
-    #return nil
+  login = login.downcase.strip
+  ary = [login]
+  unless name.nil?
+    name = name.downcase.strip
+    ary = name.split(' ').map(&:strip).reject(&:empty?) << login
+  end
+  alln = []
+  ary.each do |name|
+    alln << name
+    aname = name.to_ascii.downcase
+    alln << aname if aname != name
+  end
+  alln = alln.uniq
+  p alln
+  apikey = ENV['API_KEY']
+  alln.each do |name|
+    if $gcache.key?(name)
+        $hit += 1
+      return $gcache[name]
+    end
+    $miss += 1
+    uri = URI.parse("https://api.genderize.io?")
+    if !api_key.nil? && api_key != ''
+      uri += "apikey=#{api_key}&"
+    end
+    # name=kim&country_id=dk
+    response = Net::HTTP.get_response(uri)
+    data = JSON.parse(response.body)
+    p data
+  end
+  return nil
 end
 
 def genderize(json_file)
@@ -28,7 +59,7 @@ def genderize(json_file)
     sex = nil
     if csex.nil?
       sex = get_sex name, login, cid
-      f += 1 unless cid.nil?
+      f += 1 unless sex.nil?
     end
     user['sex'] = sex unless sex.nil?
     newj << user
--- genderize.sh
+++ genderize.sh
@@ -2,7 +2,7 @@
 if [ -z "$1" ]
 then
   echo "$0: you need to provide file.json as an arg"
-  echo "$0: ./genderize.sh github_users.json"
+  echo "$0: API_KEY=... ./genderize.sh github_users.json"
   exit 1
 fi
 ruby genderize.rb $1
