0d805d33b5e10b7a8c670d4d29d096114dc9c04f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-12 10:12:24 +0000

    Caching localization DB calls
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- geousers.sh
+++ geousers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if ( [ -z "$PG_PASS" ] || [ -z "$1" ] || [ -z "$2" ] )
+if ( [ -z "$PG_PASS" ] || [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] )
 then
-  echo "$0: you need to set password via PG_PASS=... and provide file.json file2.json as args"
-  echo "$0: PG_PASS=... ./geousers.sh github_users.json stripped.json"
+  echo "$0: you need to set password via PG_PASS=... and provide file.json file2.json file3.json as args"
+  echo "$0: PG_PASS=... ./geousers.sh github_users.json stripped.json geousers_cache.json"
   exit 1
 fi
-ruby geousers.rb $1 $2
+ruby geousers.rb $1 $2 $3
