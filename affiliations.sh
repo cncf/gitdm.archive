2b8fd96c078dd6d71e81c4506c28548bad895128
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-17 08:12:45 +0000

    new affiliations data file
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- affiliations.sh
+++ affiliations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ruby affiliations.rb affiliations.csv 2> affiliations.txt
+ruby affiliations.rb affiliations.csv github_users.json 2> affiliations.txt
 echo "Affiliations saved in affiliations.txt, you can add them to email-map via 'cat affiliations.txt >> cncf-config/email-map'"
