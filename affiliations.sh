d6cadf1a1d9ceb1b27a082b724a3e478d3a20922
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-17 10:33:53 +0000

    Eventually fixup gender and affiliation in github_users.json too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- affiliations.sh
+++ affiliations.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ruby affiliations.rb affiliations.csv github_users.json 2> affiliations.txt
-echo "Affiliations saved in affiliations.txt, you can add them to email-map via 'cat affiliations.txt >> cncf-config/email-map'"
+ruby affiliations.rb affiliations.csv github_users.json cncf-config/email-map
