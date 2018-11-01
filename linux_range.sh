7cbec7bd02479563a1a5e0caa7e639ca9065efab
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-01 07:10:20 +0000

    Updates for velocity charts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- linux_range.sh
+++ linux_range.sh
@@ -6,7 +6,7 @@ cd ../../linux/
 git config merge.renameLimit 100000
 git config diff.renameLimit 100000
 #git log --numstat -M --since "$1" --until "$2" | ~/dev/cncf/gitdm/cncfdm.py -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
-git log --numstat -M --since "$1" --until "$2" | ../cncf/gitdm/cncfdm.py -n -b ../cncf/gitdm/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
+git log --numstat -M --since "$1" --until "$2" | ~/dev/alt/gitdm/cncfdm.py -n -b ~/dev/alt/gitdm/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
 git config --unset diff.renameLimit
 git config --unset merge.renameLimit
 cd $PWD
