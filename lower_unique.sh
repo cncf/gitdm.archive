2eba4767757eb5d1fdcfdaee1424e64b8bfda712
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-26 15:59:07 +0000

    detect differences with only @ vs ! and rerun log analysis on corrected config file
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- lower_unique.sh
+++ lower_unique.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ "$1" = "" ]
+if [ "${1}" = "" ]
 then
   echo "$0: need filename argument"
   exit 1
 fi
-cp $1 out && vim -c "%s/[A-Z]/\L&/g|w|q" out && cat out | sort > out1 && cat out | sort | uniq > out2 && diff out1 out2 && rm -f out out1 out2
+grep -HIn '\w@\w' "${1}"
+cp "${1}" out && vim -c "%s/[A-Z]/\L&/g|%s/\([a-zA-Z0-9]\)@\([a-zA-Z0-9]\)/\1!\2/g|w|q" out && cat out | sort > out1 && cat out | sort | uniq > out2 && diff out1 out2 && rm -f out out1 out2
