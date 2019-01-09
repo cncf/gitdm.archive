dc29a5096ec7c138d317dcea108fc9e57ab34dba
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-09 07:37:57 +0000

    Got rid of add '*_url' JSON fields - they're not needed anymore
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ delete_json_fields.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require './merge'
+
+def strip_json(json_file, fields)
+  # Parse input JSON
+  data = JSON.parse File.read json_file
+
+  # Fields to delete
+  dels = fields.split(',').map(&:strip)
+
+  # Strip JSON
+  stripped = []
+  data.each do |row|
+    dels.each { |field| row.delete(field) }
+    stripped << row
+  end
+
+  # Write JSON back
+  pretty = JSON.pretty_generate stripped
+  File.write json_file, pretty
+end
+
+if ARGV.size < 2
+  puts "Missing arguments: file.json 'field1,field2,...,fieldN'"
+  exit(1)
+end
+
+strip_json(ARGV[0], ARGV[1])
