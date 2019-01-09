8ac87440929d85199ad551ed5a8cd184f9462374
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-09 07:37:57 +0000

    Got rid of add '*_url' JSON fields - they're not needed anymore
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ delete_json_fields.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url'
