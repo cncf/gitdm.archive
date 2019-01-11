1f8e168f7b70906c4f1889eb9bb5b0f15711b915
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-11 11:05:57 +0000

    Enhancing all_affs.csv wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- enchance_all_affs.rb
+++ enchance_all_affs.rb
@@ -6,33 +6,83 @@ require './email_code'
 require './mgetc'
 
 def enchance_all_affs(affs_file, json_file)
+  # dbg: set to true to have very verbose output
+  dbg = !ENV['DBG'].nil?
+
   # Process affiliations found by Python cncf/gitdm saved in CSV
   # "email","name","company","date_to","source"
+  # Get rid of invalid UTF-8 chars
+  contents = File.read(affs_file).scrub
+  File.write(affs_file, contents)
+
+  # Parse affiliations file
+  ln = 1
   email_affs = {}
   sources = {}
-  CSV.foreach(affs_file, headers: true) do |row|
-    next if is_comment row
-    h = row.to_h
-    e = email_encode(h['email'].strip)
-    c = h['company'].strip
-    n = h['name'].strip
-    d = h['date_to'].strip
-    s = h['source']
+  begin
+    CSV.foreach(affs_file, headers: true) do |row|
+      next if is_comment row
+      h = row.to_h
+      e = email_encode(h['email'].strip)
+      c = h['company'].strip
+      n = h['name'].strip
+      d = h['date_to'].strip
+      s = h['source']
 
-    # name --> emails mapping (one name can have multiple emails)
-    emails[n] = {} unless emails.key?(n)
-    emails[n][e] = true
+      email_affs[e] = [] unless email_affs.key?(e)
+      if d && d.length > 0
+        email_affs[e] << "#{c} < #{d}"
+      else
+        email_affs[e] << c
+      end
 
-    # affiliations by email
-    email_affs[e] = [] unless email_affs.key?(e)
-    if d && d.length > 0
-      email_affs[e] << "#{c} < #{d}"
-    else
-      email_affs[e] << c
+      sources[e] = s
+      ln += 1
     end
-
-    sources[e] = s
+  rescue => e
+    puts "CSV error on line #{ln}: #{e}"
+    binding.pry
   end
+  email_affs.each do |email, affs|
+    saffs = affs.join(', ')
+    suaffs = affs.uniq.join(', ')
+    if saffs != suaffs
+      puts "Warning: email '#{email}' has non-unique affiliations: #{affs}: '#{saffs}' != '#{suaffs}'"
+    end
+  end
+
+  # Parse JSON (only login emails connections)
+  data = JSON.parse File.read json_file
+  emails = {}
+  logins = {}
+  data.each_with_index do |user, idx|
+    e = email_encode(user['email'])
+    l = user['login'].strip
+    logins[e] = l
+    emails[l] = [] unless emails.key?(l)
+    emails[l] << e
+  end
+
+  # Check if we have all emails from JSON in our CSV
+  new_affs = {}
+  email_affs.each do |email, affs|
+      next if ['NotFound', '(Unknown)'].include?(affs.first)
+    unless logins.key?(email)
+      puts "JSON have no email '#{email}', skipping" if dbg
+      next
+    end
+    l = logins[email]
+    ems = emails[l]
+    if ems.length < 1
+      puts "Login for email '#{email}' found: '#{l}', but reverse map of emails empty"
+      next
+    end
+    ems.each do |em|
+      next if em == email
+      new_affs[em] = affs unless email_affs.key?(em)
+    end
+  end
+
   binding.pry
 end
 
