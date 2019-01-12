b7cb09096c863d7e202a00d4bf62d5e7572c7ccd
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-12 22:37:16 +0000

    intermediate
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- ghusers_partially_cached.sh
+++ ghusers_partially_cached.sh
@@ -2,5 +2,5 @@
 cp github_users.json github_users.old
 # ruby ghusers.rb rc
 ruby ghusers.rb rn
-./encode_emails.rb github_users.json temp
+#./encode_emails.rb github_users.json temp
 mv temp github_users.json
