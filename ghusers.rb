b69e90a6923c4c21aa8e9e50348970927e81807c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-16 13:57:13 +0000

    MT attempts: updated the longest running task
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- ghusers.rb
+++ ghusers.rb
@@ -86,41 +86,54 @@ def ghusers(start_date, args)
   # This is to ensure You want to continue, it displays Your limit, should be close to 5000
   # If not type 'exit-program' if Yes type 'quit' (to quit debugger & continue)
   binding.pry
+  thrs = []
+  n_thrs = Etc.nprocessors
   repos.each_with_index do |repo_name, repo_index|
-    begin
-      puts "Processing #{repo_index + 1}/#{n_repos} #{repo_name}"
-      fn = 'ghusers/' + repo_name.gsub('/', '__')
-      ofn = force_repo ? SecureRandom.hex(80) : fn
-      f = File.read(ofn)
-      puts "Got repository JSON from saved file"
-      h = JSON.parse f
-      hs << h
-    rescue Errno::ENOENT => err1
+    thrs << Thread.new do
+      h = nil
       begin
-        puts "No previously saved #{fn}, getting repo from GitHub" unless force_repo
-        if rpts <= 0
-          hint, rem, pts = rate_limit(gcs)
-          rpts = pts / 10
-          puts "Allowing #{rpts} calls without checking rate"
-        else
-          rpts -= 1
-          puts "#{rpts} calls remain before next rate check"
+        puts "Processing #{repo_index + 1}/#{n_repos} #{repo_name}"
+        fn = 'ghusers/' + repo_name.gsub('/', '__')
+        ofn = force_repo ? SecureRandom.hex(80) : fn
+        f = File.read(ofn)
+        puts "Got repository JSON from saved file"
+        h = JSON.parse f
+      rescue Errno::ENOENT => err1
+        begin
+          puts "No previously saved #{fn}, getting repo from GitHub" unless force_repo
+          if rpts <= 0
+            hint, rem, pts = rate_limit(gcs)
+            rpts = pts / 10
+            puts "Allowing #{rpts} calls without checking rate"
+          else
+            rpts -= 1
+            puts "#{rpts} calls remain before next rate check"
+          end
+          repo = gcs[hint].repo repo_name
+          h = repo.to_h
+          json = email_encode(JSON.pretty_generate(h))
+          File.write fn, json
+        rescue Octokit::TooManyRequests => err2
+          hint, td = rate_limit(gcs)
+          puts "Too many GitHub requests, sleeping for #{td} seconds"
+          sleep td
+          retry
+        rescue => err2
+          puts "Uups, somethis bad happened, check `err2` variable!"
+          STDERR.puts err2
         end
-        repo = gcs[hint].repo repo_name
-        h = repo.to_h
-        json = email_encode(JSON.pretty_generate(h))
-        File.write fn, json
-        hs << h
-      rescue Octokit::TooManyRequests => err2
-        hint, td = rate_limit(gcs)
-        puts "Too many GitHub requests, sleeping for #{td} seconds"
-        sleep td
-        retry
-      rescue => err2
-        puts "Uups, somethis bad happened, check `err2` variable!"
-        binding.pry
       end
+      h
     end
+    while thrs.length >= n_thrs
+      h = thrs.first.value
+      hs << h unless h.nil?
+      thrs = thrs[1..-1]
+    end
+  end
+  thrs.each do |thr|
+    h = thr.value
+    hs << h unless h.nil?
   end
 
   # Process each repository's commits
@@ -128,73 +141,92 @@ def ghusers(start_date, args)
   # After processed all 70 repos I had ~3900/5000 points remaining
   comms = []
   processed = {}
+  processed_mutex = Mutex.new
   n_repos = hs.count
   rpts = 0
+  thrs = []
   hs.each_with_index do |repo, repo_index|
-    begin
-      repo_name = repo['full_name'] || repo[:full_name]
-      puts "Getting commits from #{repo_index + 1}/#{n_repos} #{repo_name}"
-      if processed.key?(repo_name)
-        puts "#{repo_name} was already processed"
-        next
-      end
-      fn = 'ghusers/' + repo_name.gsub('/', '__') + '__commits'
-      ofn = force_commits ? SecureRandom.hex(80) : fn
-      f = File.read(ofn)
-      puts "Got commits JSON from saved file"
-      comm = JSON.parse f
-      if new_commits
-        author_maxdt = comm.map { |c| (c.key?('commit') && c['commit'].key?('author') && c['commit']['author'].key?('date')) ? c['commit']['author']['date'] : start_date }.max
-        committer_maxdt = comm.map { |c| (c.key?('commit') && c['commit'].key?('author') && c['commit']['author'].key?('date')) ? c['commit']['author']['date'] : start_date }.max
-        maxdt = [author_maxdt, committer_maxdt].max
-        if maxdt.nil?
-          maxdt = start_date
-        else
-          maxdt = maxdt[0...10] if maxdt.length >= 10
-        end
-        shas = {}
-        comm.each { |c| shas[c[:sha] || c['sha']] = true }
-        # hint, rem, pts = rate_limit(gcs)
-        puts "Getting new commits for #{repo_name} from #{maxdt}"
-        ocomm = commits_since(gcs, repo_name, maxdt)
-        h = ocomm.map(&:to_h)
-        nc = 0
-        h.each do |c|
-          unless shas.key?(c[:sha])
-            nc += 1
-            comm << c
-            # else: puts "#{repo_name}:#{c[:sha]} already processed"
+    thrs << Thread.new do
+      begin
+        comm = nil
+        repo_name = repo['full_name'] || repo[:full_name]
+        puts "Getting commits from #{repo_index + 1}/#{n_repos} #{repo_name}"
+        already_processed = false
+        processed_mutex.synchronize do
+          if processed.key?(repo_name)
+            puts "#{repo_name} was already processed"
+            already_processed = true
           end
         end
-        puts "Got #{nc} new commits"
-        json = email_encode(JSON.pretty_generate(comm))
-        File.write fn, json
-      end
-      comms << comm
-      processed[repo_name] = true
-    rescue Errno::ENOENT => err1
-      begin
-        from_date = start_date
-        from_date = '2012-07-01' if repo_name == 'torvalds/linux'
-        puts "No previously saved #{fn}, getting commits from GitHub from #{from_date}" unless force_commits
-        # hint, rem, pts = rate_limit(gcs)
-        comm = commits_since(gcs, repo_name, from_date)
-        h = comm.map(&:to_h)
-        puts "Got #{h.count} commits"
-        json = email_encode(JSON.pretty_generate(h))
-        File.write fn, json
+        return nil if already_processed
+        fn = 'ghusers/' + repo_name.gsub('/', '__') + '__commits'
+        ofn = force_commits ? SecureRandom.hex(80) : fn
+        f = File.read(ofn)
+        puts "Got commits JSON from saved file"
+        comm = JSON.parse f
+        if new_commits
+          author_maxdt = comm.map { |c| (c.key?('commit') && c['commit'].key?('author') && c['commit']['author'].key?('date')) ? c['commit']['author']['date'] : start_date }.max
+          committer_maxdt = comm.map { |c| (c.key?('commit') && c['commit'].key?('author') && c['commit']['author'].key?('date')) ? c['commit']['author']['date'] : start_date }.max
+          maxdt = [author_maxdt, committer_maxdt].max
+          if maxdt.nil?
+            maxdt = start_date
+          else
+            maxdt = maxdt[0...10] if maxdt.length >= 10
+          end
+          shas = {}
+          comm.each { |c| shas[c[:sha] || c['sha']] = true }
+          # hint, rem, pts = rate_limit(gcs)
+          puts "Getting new commits for #{repo_name} from #{maxdt}"
+          ocomm = commits_since(gcs, repo_name, maxdt)
+          h = ocomm.map(&:to_h)
+          nc = 0
+          h.each do |c|
+            unless shas.key?(c[:sha])
+              nc += 1
+              comm << c
+              # else: puts "#{repo_name}:#{c[:sha]} already processed"
+            end
+          end
+          puts "Got #{nc} new commits"
+          json = email_encode(JSON.pretty_generate(comm))
+          File.write fn, json
+        end
         comms << comm
-        processed[repo_name] = true
-      rescue Octokit::TooManyRequests => err2
-        hint, td = rate_limit(gcs)
-        puts "Too many GitHub requests, sleeping for #{td} seconds"
-        sleep td
-        retry
-      rescue => err2
-        puts "Uups, somethis bad happened, check `err2` variable!"
-        binding.pry
+        processed_mutex.synchronize { processed[repo_name] = true }
+      rescue Errno::ENOENT => err1
+        begin
+          from_date = start_date
+          from_date = '2012-07-01' if repo_name == 'torvalds/linux'
+          puts "No previously saved #{fn}, getting commits from GitHub from #{from_date}" unless force_commits
+          # hint, rem, pts = rate_limit(gcs)
+          comm = commits_since(gcs, repo_name, from_date)
+          h = comm.map(&:to_h)
+          puts "Got #{h.count} commits"
+          json = email_encode(JSON.pretty_generate(h))
+          File.write fn, json
+          comms << comm
+          processed_mutex.synchronize { processed[repo_name] = true }
+        rescue Octokit::TooManyRequests => err2
+          hint, td = rate_limit(gcs)
+          puts "Too many GitHub requests, sleeping for #{td} seconds"
+          sleep td
+          retry
+        rescue => err2
+          puts "Uups, somethis bad happened, check `err2` variable!"
+          STDERR.puts err2
+        end
       end
+      comm
     end
+    while thrs.length >= n_thrs
+      comm = thrs.first.value
+      comms << comm unless comm.nil?
+      thrs = thrs[1..-1]
+    end
+  end
+  thrs.each do |thr|
+    comm = thr.value
+    comms << comm unless comm.nil?
   end
   puts "Processed #{processed.keys.length} repos"
 
@@ -266,39 +298,52 @@ def ghusers(start_date, args)
   end
 
   rpts = 0
+  thrs = []
   users.each_with_index do |usr, index|
-    begin
-      if rpts <= 0
-        hint, rem, pts = rate_limit(gcs)
-        rpts = pts / 10
-        puts "Allowing #{rpts} calls without checking rate"
-      else
-        rpts -= 1
-        puts "#{rpts} calls remain before next rate check"
+    thrs << Thread.new do
+      h = nil
+      begin
+        if rpts <= 0
+          hint, rem, pts = rate_limit(gcs)
+          rpts = pts / 10
+          puts "Allowing #{rpts} calls without checking rate"
+        else
+          rpts -= 1
+          puts "#{rpts} calls remain before next rate check"
+        end
+        puts "Asking for #{index}/#{n_users}: GitHub: #{usr[1]}, email: #{usr[0]}, commits: #{usr[2]}"
+        u = nil
+        if data.key?(usr[0])
+          # Check saved JSON by email (JSON unique)
+          u = data[usr[0]]
+        else
+          # Ask GitHub by login (github unique)
+          u = gcs[hint].user usr[1]
+        end
+        u['email'] = usr[0]
+        u['commits'] = usr[2]
+        puts "Got name: #{u[:name] || u['name']}, login: #{u[:login] || u['login']}"
+        h = u.to_h
+      rescue Octokit::TooManyRequests => err2
+        hint, td = rate_limit(gcs)
+        puts "Too many GitHub requests, sleeping for #{td} seconds"
+        sleep td
+        retry
+      rescue => err2
+        puts "Uups, somethig bad happened, check `err2` variable!"
+        STDERR.puts err2
       end
-      puts "Asking for #{index}/#{n_users}: GitHub: #{usr[1]}, email: #{usr[0]}, commits: #{usr[2]}"
-      u = nil
-      if data.key?(usr[0])
-        # Check saved JSON by email (JSON unique)
-        u = data[usr[0]]
-      else
-        # Ask GitHub by login (github unique)
-        u = gcs[hint].user usr[1]
-      end
-      u['email'] = usr[0]
-      u['commits'] = usr[2]
-      puts "Got name: #{u[:name] || u['name']}, login: #{u[:login] || u['login']}"
-      h = u.to_h
-      final << h
-    rescue Octokit::TooManyRequests => err2
-      hint, td = rate_limit(gcs)
-      puts "Too many GitHub requests, sleeping for #{td} seconds"
-      sleep td
-      retry
-    rescue => err2
-      puts "Uups, somethig bad happened, check `err2` variable!"
-      binding.pry
+      h
     end
+    while thrs.length >= n_thrs
+      h = thrs.first.value
+      final << h unless h.nil?
+      thrs = thrs[1..-1]
+    end
+  end
+  thrs.each do |thr|
+    h = thr.value
+    final << h unless h.nil?
   end
 
   # Encode emails in JSON
