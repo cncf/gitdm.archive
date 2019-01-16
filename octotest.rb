94ce8c287920383186b6fd595f38838a3139c955
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-16 13:20:42 +0000

    Seconds MT attempts: processing GitHub HTTP requests in parallel - this seems to be promising
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- octotest.rb
+++ octotest.rb
@@ -1,6 +1,6 @@
 require './ghapi'
 
-gcs= octokit_init()
+gcs = octokit_init()
 puts "Client initialized, #{gcs.length} keys"
 hint, rem, pts = rate_limit(gcs)
 puts "2. Suggested client nr #{hint}: #{gcs[hint].user[:login]}, seconds till reset: #{rem}, points: #{pts}"
