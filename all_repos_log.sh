9cc533d268291a7414e8a07bcb2c28626dd1572d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-22 12:56:46 +0000

    New repos list
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all_repos_log.sh
+++ all_repos_log.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+echo 'Cleanup data'
 rm -f git_logs/*.log git_logs/*.1 git_logs/*.2
+echo 'Analyse logs'
 ./all_repos_log.rb $*
+echo 'Done'
--- ghusers_partially_cached.sh
+++ ghusers_partially_cached.sh
@@ -3,4 +3,4 @@ cp github_users.json github_users.old
 # ruby ghusers.rb rc
 ruby ghusers.rb rn
 #./encode_emails.rb github_users.json temp
-mv temp github_users.json
+#mv temp github_users.json
