87e2e8867cf270b89ca7d397265e8c72ecd89d70
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-15 08:08:40 +0000

    generated text affs files
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/scrub.rb
+++ src/scrub.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
 
+# :%s/^M//g
+# To enter ^M, type CTRL-V, then CTRL-M
+
 contents = File.read(ARGV[0]).scrub
+#contents.delete! ''
+contents.delete! "\r"
 File.write(ARGV[0], contents)
