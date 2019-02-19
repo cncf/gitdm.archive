570a99871ea0ca632ba6a669b84b746d8fcf518c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-19 12:24:29 +0000

    Regenerated text affs files
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/gen_aff_files.sh
+++ src/gen_aff_files.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ruby gen_aff_files.rb all_affs.csv
 ruby split_file.rb ../company_developers.txt ../developers_affiliations.txt
+rm ../developers_affiliations.txt ../company_developers.txt
