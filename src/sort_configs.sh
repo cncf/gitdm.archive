4c597dae97113af1bd75021e73bce80d9c1be0aa
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-20 06:40:07 +0000

    Manual corrections affter running correlation filnders
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/sort_configs.sh
+++ src/sort_configs.sh
@@ -5,3 +5,5 @@ cd ./cncf-config || exit 1
 cat email-map | sort | uniq > out; mv out email-map
 cat domain-map | sort | uniq > out; mv out domain-map
 cat aliases | sort | uniq > out; mv out aliases
+cd ..
+cat company-names-mapping | sort | uniq > out; mv out company-names-mapping
