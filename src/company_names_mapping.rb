f0cc7d9dc3e55fa57a9225678a5063f8136340f3
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-20 08:15:51 +0000

    First attempt to run auto-correcting tool (spell and upper/lower case)
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/company_names_mapping.rb
+++ src/company_names_mapping.rb
@@ -26,29 +26,39 @@ def company_names_mapping(cmap_file, config_file, csv_file, json_file)
     cmap[from] = to
   end
 
-  # Process all_affs.csv file
-  hdr = []
-  rows = []
-  CSV.foreach(csv_file, headers: true) do |row|
-    next if is_comment row
-    h = row.to_h
-    hdr = h.keys if hdr.length == 0
-    company = h['company']
-    h['source'] = 'config' if ['', nil].include?(h['source'])
-    if cmap.key?(company)
-      puts "#{h['email']}: #{company} -> #{cmap[company]}"
-      h['company'] = cmap[company]
+  # Parse JSON
+  data = JSON.parse File.read json_file
+  n_c = 0
+  n = data.count
+  comps = {}
+  data.each do |user|
+    a = user['affiliation']
+    next unless a
+    next if ['-', '', 'NotFound', '?'].include?(a)
+    affs = a.split(', ')
+    n_affs = []
+    affs.each do |aff|
+      ary = aff.split(' < ')
+      c = ary[0]
+      if cmap.key?(c)
+        nc = cmap[c]
+        if ary.length > 1
+          n_affs << "#{nc} < #{ary[1]}"
+          puts "#{user['login']}/#{user['email']}/#{ary[1]}: #{c} -> #{nc}"
+        else
+          n_affs << "#{nc}"
+          puts "#{user['login']}/#{user['email']}: #{c} -> #{nc}"
+        end
+      else
+        n_affs << aff
+      end
     end
-    rows << h
+    affs = n_affs.join(', ')
+    user['affiliation'] = affs
   end
-  CSV.open(csv_file, 'w', headers: hdr, force_quotes: true) do |csv|
-    csv << hdr
-    rows.each do |row|
-      # row = Hash[row.map { |k,v| [k,"#{v}"] }]
-      csv << row
-    end
-  end
-  exit
+  # Write JSON back
+  pretty = JSON.pretty_generate data
+  File.write json_file, pretty
 
   # Read existing mapping `cncf-config/email-map`
   existing = {}
@@ -82,6 +92,29 @@ def company_names_mapping(cmap_file, config_file, csv_file, json_file)
   end
   # Write back updated file
   File.write(config_file, lines)
+
+  # Process all_affs.csv file
+  hdr = []
+  rows = []
+  CSV.foreach(csv_file, headers: true) do |row|
+    next if is_comment row
+    h = row.to_h
+    hdr = h.keys if hdr.length == 0
+    company = h['company']
+    h['source'] = 'config' if ['', nil].include?(h['source'])
+    if cmap.key?(company)
+      puts "#{h['email']}: #{company} -> #{cmap[company]}"
+      h['company'] = cmap[company]
+    end
+    rows << h
+  end
+  CSV.open(csv_file, 'w', headers: hdr, force_quotes: true) do |csv|
+    csv << hdr
+    rows.each do |row|
+      # row = Hash[row.map { |k,v| [k,"#{v}"] }]
+      csv << row
+    end
+  end
 end
 
 if ARGV.length < 4
