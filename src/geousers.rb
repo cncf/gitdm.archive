162e83a4e0e1c748d2c03f50cba5d83edb7a646d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-13 09:44:42 +0000

    Making localize program MT
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/geousers.rb
+++ src/geousers.rb
@@ -2,25 +2,38 @@ require 'pry'
 require 'json'
 require 'pg'
 require 'unidecoder'
+require 'concurrent'
+require 'set'
+require 'thwait'
+
+$gjson_cache_filename = nil
 
 $gcache = {}
+$gcache_mtx = Concurrent::ReadWriteLock.new
+
 $hit = 0
 $miss = 0
+$gstats_mtx = Concurrent::ReadWriteLock.new
 
+# Thread safe!
 def check_stmt(c, stmt_name, args)
   begin
     key = [stmt_name, args]
+    $gcache_mtx.acquire_read_lock
     if $gcache.key?(key)
-      $hit += 1
-      return $gcache[key]
+      v = $gcache[key]
+      $gcache_mtx.release_read_lock
+      $gstats_mtx.with_write_lock { $hit += 1 }
+      return v
     end
-    $miss += 1
+    $gcache_mtx.release_read_lock
+    $gstats_mtx.with_write_lock { $miss += 1 }
     rs = c.exec_prepared stmt_name, args
     if rs.values && rs.values.count > 0
-      $gcache[key] = [rs.values.first]
+      $gcache_mtx.with_write_lock { $gcache[key] = [rs.values.first] }
       return [rs.values.first]
     end
-    $gcache[key] = []
+    $gcache_mtx.with_write_lock { $gcache[key] = [] }
   rescue PG::Error => e
     puts e
     return []
@@ -128,6 +141,7 @@ def get_cid_from_loc(c, iloc, rec, pref, suff)
   return ret
 end
 
+# Thread safe!
 def get_cid(c, loc)
   ret = get_cid_from_loc c, loc, true, '', ''
   if ret.length < 1e10
@@ -152,12 +166,14 @@ def get_cid(c, loc)
   return r[0][0].downcase, tz
 end
 
+# Not thread safe!
 def get_gcache
   ary = []
   $gcache.each { |key, val| ary << [key, val] }
   ary
 end
 
+# Not thread safe!
 def generate_global_cache(cache)
   cache.each { |key, val| $gcache[key] = val }
 end
@@ -194,6 +210,16 @@ def geousers(json_file, json_file2, json_cache, backup_freq)
   cache = JSON.parse File.read json_cache
   generate_global_cache cache
 
+  # handle CTRL+C
+  $gjson_cache_filename = json_cache
+  Signal.trap('INT') do
+    puts "Caught signal, saving cache and exiting"
+    pretty = JSON.pretty_generate get_gcache
+    File.write $gjson_cache_filename, pretty
+    puts "Saved"
+    exit 1
+  end
+
   # Process JSONs
   # Create cache from second file
   cache = {}
@@ -212,35 +238,55 @@ def geousers(json_file, json_file2, json_cache, backup_freq)
   l = 0
   f = 0
   ca = 0
+  mtx = Concurrent::ReadWriteLock.new
   all_n = data.length
+  thrs = Set[]
+  n_thrs = ENV['NCPUS'].nil? ? Etc.nprocessors : ENV['NCPUS'].to_i
   data.each_with_index do |user, idx|
-    login = user['login']
-    email = user['email']
-    loc = user['location']
-    ccid = user['country_id']
-    ctz = user['tz']
-    if (ccid.nil? || ccid == '' || ctz.nil? || ctz == '') && cache.key?([login, email])
-      rec = cache[[login, email]]
-      cid = user['country_id'] = rec['country_id']
-      tz = user['tz'] = rec['tz']
-      ca += 1
-      l += 1 if !loc.nil? && loc.length > 0
-      f += 1 unless cid.nil?
-    else
-      cid = nil
-      if (ccid.nil? || ctz.nil? || ccid == '' || ctz == '') && !loc.nil? && loc.length > 0
-        l += 1
-        cid, tz = get_cid c, loc
-        f += 1 unless cid.nil?
-        user['country_id'] = cid
-        user['tz'] = tz
+    thrs << Thread.new(user) do |usr|
+      login = usr['login']
+      email = usr['email']
+      loc = usr['location']
+      ccid = usr['country_id']
+      ctz = usr['tz']
+      ky = nil
+      $gcache_mtx.with_read_lock { ky = cache.key?([login, email]) }
+      if (ccid.nil? || ccid == '' || ctz.nil? || ctz == '') && ky
+        rec = nil
+        $gcache_mtx.with_read_lock { rec = cache[[login, email]] }
+        cid = usr['country_id'] = rec['country_id']
+        tz = usr['tz'] = rec['tz']
+        mtx.with_write_lock do
+          ca += 1
+          l += 1 if !loc.nil? && loc.length > 0
+          f += 1 unless cid.nil?
+        end
+      else
+        cid = nil
+        if (ccid.nil? || ctz.nil? || ccid == '' || ctz == '') && !loc.nil? && loc.length > 0
+          cid, tz = get_cid c, loc
+          mtx.with_write_lock do
+            l += 1
+            f += 1 unless cid.nil?
+          end
+          usr['country_id'] = cid
+          usr['tz'] = tz
+        end
+        usr['country_id'] = nil if usr['country_id'].nil?
+        usr['tz'] = nil if usr['tz'].nil?
       end
-      user['country_id'] = nil if user['country_id'].nil?
-      user['tz'] = nil if user['tz'].nil?
+      mtx.with_write_lock { n += 1 }
+      mtx.with_read_lock { puts "Row #{n}/#{all_n}: #{login}: (#{loc} -> #{cid || ccid}, #{tz || ctz}) locations #{l}, found #{f}, cache: #{ca}" }
+      usr
     end
-    newj << user
-    n += 1
-    puts "Row #{n}/#{all_n}: #{login}: (#{loc} -> #{cid || ccid}, #{tz || ctz}) locations #{l}, found #{f}, cache: #{ca}, #{$hit}/#{$miss}"
+    puts "Index: #{idx}, Hits: #{$hit}, Miss: #{$miss}"
+    while thrs.length >= n_thrs
+      tw = ThreadsWait.new(thrs.to_a)
+      t = tw.next_wait
+      usr = t.value
+      newj << usr
+      thrs = thrs.delete t
+    end
     if idx > 0 && idx % freq == 0
       pretty = JSON.pretty_generate newj
       File.write 'partial.json', pretty
@@ -249,6 +295,10 @@ def geousers(json_file, json_file2, json_cache, backup_freq)
       pretty = JSON.pretty_generate get_gcache
       File.write json_cache, pretty
     end
+  end
+  ThreadsWait.all_waits(thrs.to_a) do |thr|
+    usr = thr.value
+    newj << usr
   end
 
   # Write JSON back
