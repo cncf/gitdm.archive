3c9724c763c137aa563b52b1ae78dd80c3f2cdaa
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-14 06:38:56 +0000

    Manual updates
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/sort_configs.sh
+++ src/sort_configs.sh
@@ -7,3 +7,7 @@ cat domain-map | sort | uniq > out; mv out domain-map
 cat aliases | sort | uniq > out; mv out aliases
 cd ..
 cat company-names-mapping | sort | uniq > out; mv out company-names-mapping
+sed -i '1d' all_affs.csv
+cat all_affs.csv | sort | uniq > out
+echo '"email","name","company","date_to","source"' > all_affs.csv
+cat out >> all_affs.csv
