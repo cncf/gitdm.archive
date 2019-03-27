f50bcd8cff1447e49b898cb221685ff258dd7342
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-27 12:55:18 +0000

    Add tool that will run cncfdm.py in parallel threads and merge final results
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ src/cmd/mtp/mtp.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	// ~/dev/alt/gitdm/src/cncfdm.py -i git.log -r "^vendor/|/vendor/|^Godeps/" -R -n -b ./ -t -z -d -D -A -U -u -o all.txt -x all.csv -a all_affs.csv > all.out
+	os.Exit(1)
+}
