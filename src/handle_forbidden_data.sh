8447daa57ab52dbfa751ff5eb38517edd3064e23
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-28 11:26:56 +0000

    Updated to find forbiden data algorithm [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/handle_forbidden_data.sh
+++ src/handle_forbidden_data.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
+#find . -type f -iname "*.out" >> ./flist.txt
+#find . -type f -iname "*.htm*" >> ./flist.txt
+#find . -type f -iname "*.db" >> ./flist.txt
+#find . -type f -iname "*.dump" >> ./flist.txt
+#find . -type f -iname "*.dat" >> ./flist.txt
+#find . -type f -iname "*.old" >> ./flist.txt
 find . -type f -iname "*.csv" > ./flist.txt
 find . -type f -iname "*.txt" >> ./flist.txt
-find . -type f -iname "*.out" >> ./flist.txt
 find . -type f -iname "*.rb" >> ./flist.txt
 find . -type f -iname "*.py" >> ./flist.txt
-find . -type f -iname "*.htm*" >> ./flist.txt
 find . -type f -iname "*.md" >> ./flist.txt
 find . -type f -iname "*.json" >> ./flist.txt
 find . -type f -iname "*.sh" >> ./flist.txt
-find . -type f -iname "*.db" >> ./flist.txt
-find . -type f -iname "*.dump" >> ./flist.txt
-find . -type f -iname "*.dat" >> ./flist.txt
 find . -type f -iname "*.log" >> ./flist.txt
-find . -type f -iname "*.old" >> ./flist.txt
 find . -type f -iname "*.new" >> ./flist.txt
 find ./cncf-config -type f >> ./flist.txt
 #./handle_forbidden_data.rb `cat flist.txt`
