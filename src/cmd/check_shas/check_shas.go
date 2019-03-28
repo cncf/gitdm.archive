4a52b8fa14ba75152bf5fe58573eb4150d5d5dfd
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-28 10:36:36 +0000

    Forbiden analysis [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/cmd/check_shas/check_shas.go
+++ src/cmd/check_shas/check_shas.go
@@ -4,8 +4,13 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"regexp"
 	"runtime"
+	"sort"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -44,10 +49,42 @@ func checkSHAs(files []string) error {
 	nThreads := 0
 	runtime.GOMAXPROCS(thrN)
 	ch := make(chan string)
-	for _, file := range files {
-		go func(c chan string, fn string) {
+	lMap := make(map[string][][2]int)
+	var lMapMtx sync.Mutex
+	nFiles := len(files)
+	for idx, file := range files {
+		if idx%100 == 99 {
+			fmt.Printf("Lines analysis %d/%d\n", idx+1, nFiles)
+		}
+		go func(c chan string, i int, fn string) {
+			data, err := ioutil.ReadFile(fn)
+			if err != nil {
+				c <- fn + ": " + err.Error()
+				return
+			}
+			lines1 := strings.Split(string(data), "\r")
+			lines2 := strings.Split(string(data), "\n")
+			lines := lines1
+			if len(lines2) > len(lines1) {
+				lines = lines2
+			}
+			for j, line := range lines {
+				line = strings.TrimSpace(line)
+				if line == "" {
+					continue
+				}
+				lMapMtx.Lock()
+				v, ok := lMap[line]
+				if !ok {
+					lMap[line] = [][2]int{{i, j}}
+				} else {
+					v = append(v, [2]int{i, j})
+					lMap[line] = v
+				}
+				lMapMtx.Unlock()
+			}
 			c <- ""
-		}(ch, file)
+		}(ch, idx, file)
 		nThreads++
 		if nThreads == thrN {
 			res := <-ch
@@ -63,6 +100,64 @@ func checkSHAs(files []string) error {
 		if res != "" {
 			fmt.Printf("%s\n", res)
 		}
+	}
+	nThreads = 0
+	nonWordRE := regexp.MustCompile(`[^\w]`)
+	chs := make(chan struct{})
+	tMap := make(map[string][][3]int)
+	var tMapMtx sync.Mutex
+	for line, data := range lMap {
+		go func(c chan struct{}, l string, data [][2]int) {
+			tokens := nonWordRE.Split(l, -1)
+			toks := []string{}
+			for _, token := range tokens {
+				token = strings.TrimSpace(token)
+				if len(token) < 4 {
+					continue
+				}
+				toks = append(toks, token)
+			}
+			for i, token := range toks {
+				tMapMtx.Lock()
+				v, ok := tMap[token]
+				if !ok {
+					v = [][3]int{}
+					for _, d := range data {
+						v = append(v, [3]int{d[0], d[1], i})
+					}
+					tMap[token] = v
+				} else {
+					for _, d := range data {
+						v = append(v, [3]int{d[0], d[1], i})
+					}
+					tMap[line] = v
+				}
+				tMapMtx.Unlock()
+			}
+			c <- struct{}{}
+		}(chs, line, data)
+		nThreads++
+		if nThreads == thrN {
+			<-chs
+			nThreads--
+		}
+	}
+	for nThreads > 0 {
+		<-chs
+		nThreads--
+	}
+	keys := []string{}
+	for k := range lMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		data := lMap[k]
+		fmt.Printf("%s: ", k)
+		for _, row := range data {
+			fmt.Printf("%s:%d ", files[row[0]], row[1]+1)
+		}
+		fmt.Printf("\n")
 	}
 	return nil
 }
