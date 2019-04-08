6b33e0972fd58df1d4097d8c9b798b8d2503e69a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-08 09:41:47 +0000

    Update Linux data analysis scripts and generate new ranges
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/linux_range.sh
+++ src/linux_range.sh
@@ -2,11 +2,11 @@
 PWD=`pwd`
 FN=$PWD/linux_stats/range_$1_$2
 #cd ~/dev/linux/
-cd ../../linux/
+cd ../../../linux/
 git config merge.renameLimit 100000
 git config diff.renameLimit 100000
-#git log --numstat -M --since "$1" --until "$2" | ~/dev/cncf/gitdm/cncfdm.py -n -b ~/dev/cncf/gitdm/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
-git log --numstat -M --since "$1" --until "$2" | ~/dev/alt/gitdm/cncfdm.py -n -b ~/dev/alt/gitdm/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
+#git log --numstat -M --since "$1" --until "$2" | ~/dev/cncf/gitdm/src/cncfdm.py -n -b ~/dev/cncf/gitdm/src/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
+git log --numstat -M --since "$1" --until "$2" | ~/dev/alt/gitdm/src/cncfdm.py -n -b ~/dev/alt/gitdm/src/ -t -z -d -D -U -u -f "$1" -e "$2" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
 git config --unset diff.renameLimit
 git config --unset merge.renameLimit
 cd $PWD
