32c194504d6e5326dc8eb9131042cd9fec35b0e8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-09 11:52:35 +0000

    Update repo_in_range.sh script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/repo_in_range.sh
+++ src/repo_in_range.sh
@@ -5,6 +5,6 @@ cd "$1"
 echo "Processing repo $1 $2 $3 $4"
 git config merge.renameLimit 100000
 git config diff.renameLimit 100000
-git log --numstat -M --since "$3" --until "$4" | ~/dev/alt/gitdm/cncfdm.py -n -b ~/dev/alt/gitdm/ -t -z -d -D -U -u -f "$3" -e "$4" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
+git log --numstat -M --since "$3" --until "$4" | ~/dev/alt/gitdm/src/cncfdm.py -n -b ~/dev/alt/gitdm/src/ -t -z -d -D -U -u -f "$3" -e "$4" -h $FN.html -o $FN.txt -x $FN.csv > $FN.out
 git config --unset diff.renameLimit
 git config --unset merge.renameLimit
