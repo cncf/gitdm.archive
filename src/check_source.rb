3d0db88c65e9419f467246bc32e8591560173e39
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-16 06:23:27 +0000

    Source update tool
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ src/check_source.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+#
+require 'pry'
+require './merge'
+
+def check_source(json_file)
+  # Parse input JSON
+  data = JSON.parse File.read json_file
+
+  # Delete fields from JSON
+  stripped = []
+  data.each do |row|
+    s = row['source']
+    a = row['affiliation']
+    if s == 'notfound' && a != 'NotFound'
+      row['source'] = 'config'
+      puts "No longer not found: #{row['login']}/#{row['email']}: '#{a}'"
+    end
+    stripped << row
+  end
+
+  # Write JSON back
+  pretty = JSON.pretty_generate stripped
+  File.write json_file, pretty
+end
+
+if ARGV.size < 1
+  puts "Missing arguments: file.json"
+  exit(1)
+end
+
+check_source(ARGV[0])
