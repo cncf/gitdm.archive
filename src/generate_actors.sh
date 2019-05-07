ffdafd124c17a1438bf56aace4238d493595cc23
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-07 06:09:52 +0000

    Adding OpenEBS project
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors.sh
+++ src/generate_actors.sh
@@ -38,6 +38,7 @@ sudo -u postgres psql -tA kubeedge < ~/dev/go/src/github.com/cncf/devstats/util_
 sudo -u postgres psql -tA brigade < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
 sudo -u postgres psql -tA crio < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
 sudo -u postgres psql -tA networkservicemesh < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
+sudo -u postgres psql -tA openebs < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
 sudo -u postgres psql -tA cncf < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
 sudo -u postgres psql -tA allprj < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
 sudo -u postgres psql -tA opencontainers < ~/dev/go/src/github.com/cncf/devstats/util_sql/actors.sql >> actors.txt
