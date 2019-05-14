5d66575d832dc647b6c6384c6ede0c021ac8981e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-14 14:28:38 +0000

    Actors generate wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors_gql.sh
+++ src/generate_actors_gql.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm-graphql/devstats-helm-graphql/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm-graphql/devstats-helm-graphql/secrets/PG_PASS.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_graphql.sh > ~/dev/alt/gitdm/src/actors_gql.txt
+PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm-graphql/devstats-helm-graphql/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm-graphql/devstats-helm-graphql/secrets/PG_PASS.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_gql.sh > ~/dev/alt/gitdm/src/actors_gql.txt
 sed -i '/pod "devstats-actors/d' actors_gql.txt
 cat actors_gql.txt | sort | uniq > out && mv out actors_gql.txt
