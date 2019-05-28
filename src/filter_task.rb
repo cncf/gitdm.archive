a23ebca01d6eeeb34d37c65e190400d79b1a17b6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-28 13:16:55 +0000

    Adding more smart filter tools
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/filter_task.rb
+++ src/filter_task.rb
@@ -1,6 +1,26 @@
 #!/usr/bin/env ruby
 
-if ARGV.size < 2
-  puts "Missing argument: unknowns.txt|alldevs.txt unknown_with_xyz.json"
+require 'json'
+
+if ARGV.size < 3
+  puts "Missing argument: unknowns.txt|alldevs.txt unknown_with_xyz.json output.txt"
   exit(1)
+end
+
+email2gh = {}
+gh = JSON.parse File.read ARGV[1]
+gh.each do |user|
+  email = user['email']
+  email2gh[email] = true
+end
+
+open(ARGV[2], 'w') do |f|
+  File.readlines(ARGV[0]).each do |line|
+    line.strip!
+    ary = line.split "\t"
+    email = ary[1]
+    if email2gh.key?(email)
+      f.puts line
+    end
+  end
 end
