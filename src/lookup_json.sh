6c906eb2f05f207635a377296eae329b8a7c6bf2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-28 13:16:55 +0000

    Adding more smart filter tools
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/lookup_json.sh
+++ src/lookup_json.sh
@@ -5,9 +5,11 @@ ruby lookup_json.rb github_users.json affiliation '/^\(Unknown\)$/' all_unknown.
 # Devs with Unknown affiliation having case insensitive "linkedin" in their blog or bio or location:
 rm -f unknown_with_linkedin.json unknown_with_linkedin.date
 ruby lookup_json.rb github_users.json ':any?, blog, location, bio' '/linkedin/i' affiliation '/^\(Unknown\)$/' unknown_with_linkedin.json
+./filter_task.rb unknowns.txt unknown_with_linkedin.json unknowns_with_linkedin.txt
 # Devs with Unknown affiliation having blog property non-empty
 rm -f unknown_with_blog.json unknown_with_blog.dat
 ruby lookup_json.rb github_users.json blog '/[^\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_blog.json
+./filter_task.rb unknowns.txt unknown_with_blog.json unknowns_with_blog.txt
 # Devs with Unknown affiliation having location & name property non-empty
 rm -f unknown_with_location_and_name.json unknown_with_location_and_name.dat
 ruby lookup_json.rb github_users.json 'location,name' '/[^\s]+/' affiliation '/^\(Unknown\)$/' unknown_with_location_and_name.json
