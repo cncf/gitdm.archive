8e8646710df020f3d3978d9bd2b41138defa99de
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-13 15:42:49 +0000

    Actors wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors_nonlf.sh
+++ src/generate_actors_nonlf.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_PASS.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_nonlf.sh > ~/dev/alt/gitdm/src/actors_nonlf.txt
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to set PG_PASS env variable"
+  exit 1
+fi
+PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_HOST.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_nonlf.sh > ~/dev/alt/gitdm/src/actors_nonlf.txt
 sed -i '/pod "devstats-actors/d' actors_nonlf.txt
 cat actors_nonlf.txt | sort | uniq > out && mv out actors_nonlf.txt
