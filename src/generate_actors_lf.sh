53abe9251a386d9f02071fe8d027a3dac43b4794
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-13 06:46:21 +0000

    generated LF actors
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors_lf.sh
+++ src/generate_actors_lf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_PASS.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_lf.sh > ~/dev/alt/gitdm/src/actors_lf.txt
+PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_PASS.dev.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_lf.sh > ~/dev/alt/gitdm/src/actors_lf.txt
 sed -i '/pod "devstats-actors/d' actors_lf.txt
 cat actors_lf.txt | sort | uniq > out && mv out actors_lf.txt
 ./scrub.rb actors_lf.txt
