dba0b05fecad30807cd2e7d69429199ab9ea4455
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-02 07:55:28 +0000

    Addin gh tool to get single user GH JSON entry when GH id is known
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ src/gh.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'octokit'
+require 'json'
+require 'concurrent'
+require 'unidecoder'
+require 'pg'
+
+require './ghapi'
+require './genderize_lib'
+require './geousers_lib'
+
+gcs = octokit_init()
+hint = rate_limit(gcs)[0]
+u = gcs[hint].user ARGV[0]
+h = u.to_h
+
+init_sqls()
+
+h[:country_id], h[:tz] = get_cid h[:location]
+h[:sex], h[:sex_prob], ok = get_sex h[:name], h[:login], h[:country_id]
+
+h[:commits] = 0
+h[:affiliation] = "(Unknown)"
+h[:email] = "change-me"
+h[:source] = "config"
+
+print JSON.pretty_generate(h) + "\n"
