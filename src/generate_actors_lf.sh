ca36b0fe2a1edc01bf8b50812a8e7d2f8b390268
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-19 07:41:07 +0000

    Update LF actors script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors_lf.sh
+++ src/generate_actors_lf.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+if [ -z "$KUBECONFIG" ]
+then
+  echo "$0: you should set KUBECONFIG env variable, using /root/.kube/config_lf instead"
+  export KUBECONFIG='/root/.kube/config_lf'
+fi
+if [ -z "$AWS_PROFILE" ]
+then
+  echo "$0: you must set AWS_PROFILE env variable"
+  exit 1
+fi
 PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_HOST.secret`" PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm-lf/devstats-helm/secrets/PG_PASS.dev.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_lf.sh > ~/dev/alt/gitdm/src/actors_lf.txt
 sed -i '/pod "devstats-actors/d' actors_lf.txt
 cat actors_lf.txt | sort | uniq > out && mv out actors_lf.txt
