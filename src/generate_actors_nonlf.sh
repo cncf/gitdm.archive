c1f5baa0ef334e77ba8e0ec110b5246f14177107
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-19 07:36:39 +0000

    Fix non-lf authors script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/generate_actors_nonlf.sh
+++ src/generate_actors_nonlf.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 if [ -z "$PG_PASS" ]
 then
-  echo "$0: you need to set PG_PASS env variable"
-  exit 1
+  echo "$0: you should set PG_PASS env variable, using ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_HOST.secret instead"
+  export PG_PASS="`cat ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_PASS.secret`"
+fi
+if [ -z "$KUBECONFIG" ]
+then
+  echo "$0: you should set KUBECONFIG env variable, using /root/.kube/config_cncf instead"
+  export KUBECONFIG='/root/.kube/config_cncf'
 fi
 PG_HOST="`cat ~/dev/go/src/github.com/cncf/devstats-helm/devstats-helm/secrets/PG_HOST.secret`" ~/dev/go/src/github.com/cncf/devstats/devel/k8s_generate_actors_nonlf.sh > ~/dev/alt/gitdm/src/actors_nonlf.txt
 sed -i '/pod "devstats-actors/d' actors_nonlf.txt
