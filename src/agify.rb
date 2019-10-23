f745a0e28d051eac235ef8de878bab832894019c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-23 12:55:39 +0000

    Fix bug and display more during 1st attempts to collect data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/agify.rb
+++ src/agify.rb
@@ -95,6 +95,7 @@ def agify(json_file, json_file2, json_cache, backup_freq)
         age = nil
         if cage.nil?
           age, ok = get_age name, login, cid
+          puts "Got #{name}, #{login}, #{cid} -> #{age}, #{ok}" unless age.nil?
           mtx.with_write_lock { f += 1 unless age.nil? }
           usr['age'] = age
         end
