871defa2dee25dea73450b45bf2211dba4387d8b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-23 13:25:04 +0000

    new field to keep in final JSON
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/check_json_fields.sh
+++ src/check_json_fields.sh
@@ -4,12 +4,12 @@ then
   echo "$0: you need to provide JSON filename as an argument"
   exit 1
 fi
-fields='login,id,node_id,type,site_admin,name,company,blog,location,email,hireable,bio,public_repos,public_gists,followers,following,created_at,updated_at,commits,affiliation,country_id,tz,sex,sex_prob,source'
+fields='login,id,node_id,type,site_admin,name,company,blog,location,email,hireable,bio,public_repos,public_gists,followers,following,created_at,updated_at,commits,affiliation,country_id,tz,sex,sex_prob,source,age'
 if [ ! -z "$2" ]
 then
   if [ "$2" = "small" ]
   then
-    fields='login,email,affiliation,source,name,commits,location,country_id,sex,sex_prob,tz'
+    fields='login,email,affiliation,source,name,commits,location,country_id,sex,sex_prob,tz,age'
   else
     fields="$2"
   fi
