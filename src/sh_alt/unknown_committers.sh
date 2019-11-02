eb24d97bc8455d1d543911d34e2b8aaa9cfb1074
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-02 09:35:19 +0000

    Run CII stats without working old lg server [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ src/sh_alt/unknown_committers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to set PG_PASS=..."
+  exit 1
+fi
+PG_DB=cii GHA2DB_LOCAL=1 GHA2DB_CSVOUT=unknown_committers.csv ./runq sql/unknown_committers.sql {{exclude_bots}} "`cat sql/exclude_bots.sql`"
