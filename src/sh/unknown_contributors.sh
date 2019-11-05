25ee4424b7508c1b569b86f8a2550fd22dfe0052
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-05 06:27:42 +0000

    Skip dates in output [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/sh/unknown_contributors.sh
+++ src/sh/unknown_contributors.sh
@@ -4,4 +4,4 @@ then
   echo "$0: you need to set PG_PASS=..."
   exit 1
 fi
-PG_DB=cii GHA2DB_LOCAL=1 GHA2DB_CSVOUT=unknown_contributors.csv runq sql/unknown_contributors.sql {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`"
+PG_DB=cii GHA2DB_SKIPTIME=1 GHA2DB_LOCAL=1 GHA2DB_CSVOUT=unknown_contributors.csv runq sql/unknown_contributors.sql {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`"
