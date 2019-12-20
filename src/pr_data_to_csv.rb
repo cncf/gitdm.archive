71cd5a72787a9e61c37cc12e900aaea719cb6172
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-20 07:23:18 +0000

    Merge complex PR tool wip 2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/pr_data_to_csv.rb
+++ src/pr_data_to_csv.rb
@@ -5,21 +5,29 @@ require 'pry'
 
 require './email_code'
 
-def pr_data_to_csv(pr_data_file)
+def process_affiliation(uname, name, emails, companies)
+  # type,email,name,github,linkedin1,linkedin2,linkedin3,commits,gender,location,affiliations,new emails
+  ['(Unknown)', emails.join(', '), uname, "https://github.com/#{name}", '', '', '', '0', '', '', companies.join(','), '']
+end
+
+def pr_data_to_csv(pr_data_file, csv_file)
   companies = []
   emails = []
   name = ''
+  uname = ''
   n = 0
+  data = []
   File.readlines(pr_data_file).each do |line|
     n += 1
     line = email_encode(line.delete(" \t\r\n"))
     ary = line.split(':')
     if ary.length == 2
       if companies.length > 0
-        # p [name, emails, companies]
+        data << process_affiliation(uname, name, emails, companies)
       end
       companies = []
       name = ary[0]
+      uname = name
       emails = ary[1].split(',')
       emails.each do |email|
         ary2 = email.split('!')
@@ -41,7 +49,13 @@ def pr_data_to_csv(pr_data_file)
       aff = ary[0]
       ary2 = aff.split('<')
       if ary2.length > 2
-        puts "Invalid affiliation config, line #{n}: #{line}"
+        puts "Invalid affiliation config (more than one <), line #{n}: #{line}"
+        binding.pry
+        next
+      end
+      ary2 = aff.split(',')
+      if ary2.length > 1
+        puts "Invalid affiliation config (contains ,), line #{n}: #{line}"
         binding.pry
         next
       end
@@ -52,11 +66,21 @@ def pr_data_to_csv(pr_data_file)
       next
     end
   end
+  if companies.length > 0
+    data << process_affiliation(uname, name, emails, companies)
+  end
+  puts "Writting #{csv_file}..."
+  hdr = %w(type email name github linkedin1 linkedin2 linkedin3 commits gender location affiliations)
+  hdr << 'new emails'
+  CSV.open(csv_file, 'w', headers: hdr) do |csv|
+    csv << hdr
+    data.each { |row| csv << row }
+  end
 end
 
-if ARGV.size < 1
-  puts "Missing arguments: pr_data.txt"
+if ARGV.size < 2
+  puts "Missing arguments: pr_data.txt output.csv"
   exit(1)
 end
 
-pr_data_to_csv(ARGV[0])
+pr_data_to_csv(ARGV[0], ARGV[1])
