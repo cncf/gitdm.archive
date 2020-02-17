c422cc20dff700358b0ed9d60d2626552c3a87de
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-17 21:06:08 +0000

    More info in script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/post_manual_updates.sh
+++ src/post_manual_updates.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo "delete json fields"
 ./delete_json_fields.sh github_users.json
+echo "sort json"
 ./sort_json.rb github_users.json
+echo "unique json"
 ./unique_json.rb github_users.json
+echo "strip json"
 ./strip_json.sh github_users.json stripped.json
+echo "affiliated json"
 ONLY_AFF=1 ./strip_json.sh github_users.json affiliated.json
 cp affiliated.json ~/dev/go/src/github.com/cncf/devstats/github_users.json
+echo "gen text affs files"
 ./gen_aff_files.sh
