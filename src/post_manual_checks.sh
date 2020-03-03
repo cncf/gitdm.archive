c3f7f2aafb5f07f19a22172e9eb58c815093b000
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-03 17:31:54 +0000

    Other various files
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/post_manual_checks.sh
+++ src/post_manual_checks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "sort configs"
+./sort_configs.sh
+echo "company names mapping"
 ./company_names_mapping.sh
+echo "lower unique"
 ./lower_unique.sh cncf-config/email-map
+echo "json fields"
 ./check_json_fields.sh github_users.json
+echo "forbidden data"
 ./handle_forbidden_data.sh
