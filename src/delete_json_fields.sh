74d8515116001a4b3e9c7bd2be3e6ea61af1c5a4
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-08-01 05:53:14 +0000

    Remove tz info
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/delete_json_fields.sh
+++ src/delete_json_fields.sh
@@ -4,4 +4,4 @@ then
   echo "$0: you need to provide JSON filename as an argument"
   exit 1
 fi
-ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age'
+ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz'
