0f7498c532add255a7e578241f61169ec2ae0e60
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-12 14:23:24 +0000

    Add mappings from DA-SFDC
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/company_names_mapping.sh
+++ src/company_names_mapping.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./sort_configs.sh
 ruby ./company_names_mapping.rb company-names-mapping cncf-config/email-map all_affs.csv github_users.json
+echo "Consider 'ruby ./company_names_mapping.rb da-company-names-mapping.txt cncf-config/email-map all_affs.csv github_users.json'"
+#ruby ./company_names_mapping.rb da-company-names-mapping.txt cncf-config/email-map all_affs.csv github_users.json
