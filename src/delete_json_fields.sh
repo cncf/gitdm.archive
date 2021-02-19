1aa34a98eb557815df0218ad151daaf1d5a850b7
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-02-19 09:14:38 +0000

    Post merge updates
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/delete_json_fields.sh
+++ src/delete_json_fields.sh
@@ -4,4 +4,5 @@ then
   echo "$0: you need to provide JSON filename as an argument"
   exit 1
 fi
-ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz'
+#ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz'
+ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz,type,site_admin,hireable,created_at,update_at'
