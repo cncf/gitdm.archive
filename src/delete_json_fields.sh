6f43630bac54bafdb064913f18a5c67b9937b6a0
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-18 04:37:33 +0000

    Post merge updates
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/delete_json_fields.sh
+++ src/delete_json_fields.sh
@@ -5,4 +5,4 @@ then
   exit 1
 fi
 #ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz'
-ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz,type,site_admin,hireable,created_at,update_at'
+ruby delete_json_fields.rb "$1" 'avatar_url,gravatar_id,url,html_url,followers,following,followers_url,following_url,gists_url,starred_url,subscriptions_url,organizations_url,repos_url,events_url,received_events_url,emails,twitter_username,sex,sex_prob,age,tz,type,site_admin,hireable,created_at,update_at,updated_at,public_repos,public_gists'
