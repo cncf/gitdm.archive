198c403e39fef92ba33a6012942dcaf1e5e6777a
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-08-30 11:55:42 +0000

    Final company mapping updates
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/post_manual_updates.sh
+++ src/post_manual_updates.sh
@@ -13,7 +13,7 @@ echo "strip json"
 ./strip_json.sh github_users.json stripped.json
 echo "affiliated json"
 ONLY_AFF=1 ./strip_json.sh github_users.json affiliated.json
-cp affiliated.json ~/dev/go/src/github.com/cncf/devstats/github_users.json
+cp affiliated.json ../../devstats/github_users.json
 echo "gen text affs files"
 ./gen_aff_files.sh
 echo "consider ./check_spell and ./map_orgs.sh"
