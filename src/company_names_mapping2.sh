0febfbcb9fcd56c6742e03b44a3f8c7e02f2fc45
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-09-02 08:41:09 +0000

    Scripts updated
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/company_names_mapping2.sh
+++ src/company_names_mapping2.sh
@@ -8,4 +8,9 @@ then
   mysql -e "create user 'u' identified by 'p'"
   export SH_DSN='u:p@tcp(127.0.0.1:3306)/?charset=utf8'
 fi
-CACHED=1 TRUNC='' NO_ACQS=1 ./map_orgs && mv config.txt cncf-config/email-map && mv mapped.json github_users.json
+if [ ! -z "$MANUAL" ]
+then
+  ./map_orgs
+else
+  CACHED=1 TRUNC='' NO_ACQS=1 ./map_orgs && mv config.txt cncf-config/email-map && mv mapped.json github_users.json
+fi
