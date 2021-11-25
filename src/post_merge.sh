8b932c030fe3783b12f0b378db996641d2d653b0
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-11-25 09:40:14 +0000

    script to do post merge updateswq
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ src/post_merge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "$0: you need to specify PG_PASS=..."
+  exit 1
+fi
+if [ -z "${BRANCH}" ]
+then
+  export BRANCH='HEAD^'
+fi
+git diff "${BRANCH}" ../*.txt > input.diff
+vim input.diff
+./update_from_pr_diff.rb ./input.diff github_users.json cncf-config/email-map || exit 2
+FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh
