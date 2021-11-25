f4aebf2df2b2367d5ceb936f7c1ed75fd34af4af
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-11-25 09:42:44 +0000

    Script update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/post_merge.sh
+++ src/post_merge.sh
@@ -11,4 +11,5 @@ fi
 git diff "${BRANCH}" ../*.txt > input.diff
 vim input.diff
 ./update_from_pr_diff.rb ./input.diff github_users.json cncf-config/email-map || exit 2
+git diff
 FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh
