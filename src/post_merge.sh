cb63dbbd30995ba06ae5467332e6048c852b37e9
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-01-13 06:34:24 +0000

    Manual post merge updates
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/post_merge.sh
+++ src/post_merge.sh
@@ -12,5 +12,8 @@ git diff "${BRANCH}" ../*.txt > input.diff
 vim input.diff
 ./update_from_pr_diff.rb ./input.diff github_users.json cncf-config/email-map || exit 2
 git diff
-FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh
-git diff
+if [ -z "$PARTIAL" ]
+then
+  FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh
+  git diff
+fi
