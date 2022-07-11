b0cee92afe47bf43ab8c9d1d8692fbb3f85c5165
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2022-07-11 09:31:07 +0000

    New login_contributions.csv & login_contributions.sh
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- src/login_contributions.sh
+++ src/login_contributions.sh
@@ -6,3 +6,12 @@ then
 else
   kubectl exec -n devstats-prod devstats-postgres-0 -- psql allprj --csv -c "select dup_actor_login as login, count(id) as cnt from gha_events where type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent') group by dup_actor_login order by cnt desc" > login_contributions.csv
 fi
+./check_shas login_contributions.csv
+echo -n "Proceed (y/n)? "
+read ans
+if [ ! "${ans}" = "y" ]
+then
+  echo "Fix forbiden SHAs and then run './update_login_contributions.rb && FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh' manually"
+  exit 1
+fi
+./update_login_contributions.rb && FULL=1 ./post_manual_checks.sh && ./post_manual_updates.sh
