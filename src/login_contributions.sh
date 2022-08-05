f6bd5449a7cfe505f76206a20d03faa87149f18b
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-08-05 05:04:10 +0000

    Fix the script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/login_contributions.sh
+++ src/login_contributions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TESTSRV=1
-if [ -z "${TESTSRV}" ]
+if [ ! -z "${TESTSRV}" ]
 then
   kubectl exec -n devstats-test devstats-postgres-0 -- psql allprj --csv -c "select dup_actor_login as login, count(id) as cnt from gha_events where type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent') group by dup_actor_login order by cnt desc" > login_contributions.csv
 else
