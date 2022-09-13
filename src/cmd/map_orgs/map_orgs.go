b716f50024ad2c047bc8328277bc19e698f48827
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-09-13 08:09:26 +0000

    Post merge updates
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/cmd/map_orgs/map_orgs.go
+++ src/cmd/map_orgs/map_orgs.go
@@ -378,6 +378,7 @@ func addHardcodedMaps(maps map[string]string) (nH int) {
 		{"Microsoft", "Microsoft Corporation"},
 		{"Spotify", "Spotify AB"},
 		{"Intuit", "Intuit Inc."},
+		{"Datadog", "Datadog Inc"},
 	}
 	for _, req := range required {
 		val, ok := maps[req[0]]
